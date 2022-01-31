ea52c7cfb233d0f3d07868a17879df5de1f2d79f
Jesper Justesen <jesper.justesen@gmail.com>
2022-01-31 15:30:40 +0100

    Add SQL scripts for seeding
    also implemented code to handle environment based scripts

--- source/Energinet.DataHub.ActorRegistry.ApplyDBMigrationsApp/Helpers/EnvironmentFilter.cs
+++ source/Energinet.DataHub.ActorRegistry.ApplyDBMigrationsApp/Helpers/EnvironmentFilter.cs
@@ -13,18 +13,37 @@
 // limitations under the License.
 
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Energinet.DataHub.ActorRegistry.ApplyDBMigrationsApp.Helpers
 {
     public static class EnvironmentFilter
     {
+        private static readonly string[] _validEnvironments = { "u-001", "u-002", "t-001", "t-002", "b-001", "b-002" };
+
         public static Func<string, bool> GetFilter(string[] args)
         {
+            var environment = GetEnvironmentArgument(args).Replace("-", "_").ToUpper();
+            if (string.IsNullOrEmpty(environment))
+            {
+                return file => file.EndsWith(".sql", StringComparison.OrdinalIgnoreCase) &&
+                               ((file.Contains(".Scripts.Seed.", StringComparison.OrdinalIgnoreCase) && args.Contains("includeSeedData")) ||
+                                (file.Contains(".Scripts.Test.", StringComparison.OrdinalIgnoreCase) && args.Contains("includeTestData")) ||
+                                file.Contains(".Scripts.Model.", StringComparison.OrdinalIgnoreCase));
+            }
+
             return file => file.EndsWith(".sql", StringComparison.OrdinalIgnoreCase) &&
-                           ((file.Contains(".Scripts.Seed.", StringComparison.OrdinalIgnoreCase) && args.Contains("includeSeedData")) ||
-                            (file.Contains(".Scripts.Test.", StringComparison.OrdinalIgnoreCase) && args.Contains("includeTestData")) ||
-                            file.Contains(".Scripts.Model.", StringComparison.OrdinalIgnoreCase));
+                           ((file.Contains($".Scripts.{environment}.Seed.", StringComparison.OrdinalIgnoreCase) && args.Contains("includeSeedData")) ||
+                            (file.Contains($".Scripts.{environment}.Test.", StringComparison.OrdinalIgnoreCase) && args.Contains("includeTestData")) ||
+                            file.Contains($".Scripts.{environment}.Model.", StringComparison.OrdinalIgnoreCase));
+        }
+
+        private static string GetEnvironmentArgument(IReadOnlyList<string> args)
+        {
+            return args.Count > 1 && _validEnvironments.Contains(args[1])
+                ? args[1]
+                : string.Empty;
         }
     }
 }
