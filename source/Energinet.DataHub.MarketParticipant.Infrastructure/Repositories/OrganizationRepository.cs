2c5d14f4d55bb916d9e01aa00328a096f1d60c56
Jesper Justesen <jesper.justesen@gmail.com>
2022-02-09 10:31:25 +0100

    Add Infrastructure and repository

---
+++ source/Energinet.DataHub.MarketParticipant.Infrastructure/Repositories/OrganizationRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Energinet.DataHub.MarketParticipant.Domain.Model;
+using Energinet.DataHub.MarketParticipant.Domain.Repositories;
+
+namespace Energinet.DataHub.MarketParticipant.Infrastructure.Repositories
+{
+    public class OrganizationRepository : IOrganizationRepository
+    {
+        public Task SaveAsync(Organization organization)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
