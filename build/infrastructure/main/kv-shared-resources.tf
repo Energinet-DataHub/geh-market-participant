e665ad40574d86975572f27e07dc38d684da300b
Jesper Justesen <jesper.justesen@gmail.com>
2022-02-11 08:38:05 +0100

    Create function app and skeleton (#12)
    * Added function app project
    
    * Update Energinet.DataHub.MarketParticipant.EntryPoint.Organization.csproj
    
    Added nullable enable to csporj
    
    * Add Domain project
    
    * Add Interface for Organization Repository
    
    Also update a few properties on the DTO
    
    * Add Simple Injector
    
    * Add Infrastructure and repository
    
    * Added migrations for Organization table
    
    * Add unit tests
    
    * Started DB implementation
    
    * Basic Skeleton complete
    
    * Added missing copyrights
    
    * Create local.settings.sample.json
    
    * Added queryhelper
    
    * Aligned langVersion
    
    * Remove implicit usings
    
    * Update Energinet.DataHub.MarketParticipant.Utilities.csproj
    
    * Add Azure func terraform
    
    * add missing job in ci.yml
    
    * Update prerelease job in ci.yml
    
    * Update workflow versions
    
    Add missing modules to terraform
    
    * Add sealed to classes missing it
    
    Co-authored-by: Mikkel Busk Espersen <xmbes@energinet.dk>
    Co-authored-by: Mikkel Busk <mikkel_busk@hotmail.com>
    Co-authored-by: michaelmajgaard <52096428+michaelmajgaard@users.noreply.github.com>

--- build/infrastructure/main/kv-shared-resources.tf
+++ build/infrastructure/main/kv-shared-resources.tf
@@ -16,6 +16,11 @@ data "azurerm_key_vault" "kv_shared_resources" {
   resource_group_name = var.shared_resources_resource_group_name
 }
 
+data "azurerm_key_vault_secret" "appi_instrumentation_key" {
+  name         = "appi-shared-instrumentation-key"
+  key_vault_id = data.azurerm_key_vault.kv_shared_resources.id
+}
+
 data "azurerm_key_vault_secret" "mssql_data_admin_name" {
   name         = "mssql-data-admin-user-name"
   key_vault_id = data.azurerm_key_vault.kv_shared_resources.id
---
+++ source/Energinet.DataHub.MarketParticipant.Infrastructure/Repositories/OrganizationRepository.cs
@@ -0,0 +1,39 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using Energinet.DataHub.MarketParticipant.Domain.Model;
+using Energinet.DataHub.MarketParticipant.Domain.Repositories;
+using Dapper;
+
+namespace Energinet.DataHub.MarketParticipant.Infrastructure.Repositories
+{
+    public sealed class OrganizationRepository : IOrganizationRepository
+    {
+        private readonly ActorDbConfig _actorDbConfig;
+
+        public OrganizationRepository(ActorDbConfig actorDbConfig)
+        {
+            _actorDbConfig = actorDbConfig;
+        }
+
+        public async Task SaveAsync(Organization organization)
+        {
+            await using var connection = new SqlConnection(_actorDbConfig.ConnectionString);
+            await connection.OpenAsync().ConfigureAwait(false);
+            await connection.InsertAsync(organization).ConfigureAwait(false);
+        }
+    }
+}
