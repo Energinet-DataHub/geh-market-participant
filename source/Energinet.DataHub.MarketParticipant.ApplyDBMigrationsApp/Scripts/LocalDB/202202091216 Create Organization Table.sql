c308b9fb36c0baeb822563064e4c3329cba1554f
Alexei Mihalchuk <AIU@energinet.dk>
2022-02-18 11:26:43 +0100

    Added ActorId in wrong script.

--- source/Energinet.DataHub.MarketParticipant.ApplyDBMigrationsApp/Scripts/LocalDB/202202091216 Create Organization Table.sql
+++ source/Energinet.DataHub.MarketParticipant.ApplyDBMigrationsApp/Scripts/LocalDB/202202091216 Create Organization Table.sql
@@ -5,7 +5,6 @@ GO
 CREATE TABLE [dbo].[OrganizationInfo]
 (
     [Id]   [uniqueidentifier] NOT NULL,
-    [ActorId]   [uniqueidentifier] NOT NULL,
     [Name] [nvarchar](max)    NOT NULL,
     [Gln]  [nvarchar](50)     NOT NULL
         CONSTRAINT [PK_OrganizationInfo] PRIMARY KEY CLUSTERED
