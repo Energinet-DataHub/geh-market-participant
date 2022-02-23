e23494769c03913b1d8207730b100f360f049dc1
Alexei Mihalchuk <AIU@energinet.dk>
2022-02-23 09:53:00 +0100

    Moved again.

--- source/Energinet.DataHub.MarketParticipant.Tests/Handlers/TestA.cs
+++ source/Energinet.DataHub.MarketParticipant.Tests/Handlers/TestA.cs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-namespace Energinet.DataHub.MarketParticipant.Tests
+namespace Energinet.DataHub.MarketParticipant.Tests.Handlers
 {
     public sealed class CreateOrganizationHandlerTests
     {
