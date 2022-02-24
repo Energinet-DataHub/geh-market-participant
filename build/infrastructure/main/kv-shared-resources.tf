bc36a63bd9b9464e505fce84637938f656277f39
Daniel Bjerring Jørgensen <dbj1986dk@gmail.com>
2022-02-24 15:10:33 +0100

    use shared asp + rename func tf file

--- build/infrastructure/main/kv-shared-resources.tf
+++ build/infrastructure/main/kv-shared-resources.tf
@@ -50,3 +50,8 @@ data "azurerm_key_vault_secret" "sbt-market-participant-changed-name" {
   name         = "sbt-market-participant-changed-name"
   key_vault_id = data.azurerm_key_vault.kv_shared_resources.id
 }
+
+data "azurerm_key_vault_secret" "plan_shared_id" {
+  name         = "plan-shared-id"
+  key_vault_id = data.azurerm_key_vault.kv_shared_resources.id
+}
