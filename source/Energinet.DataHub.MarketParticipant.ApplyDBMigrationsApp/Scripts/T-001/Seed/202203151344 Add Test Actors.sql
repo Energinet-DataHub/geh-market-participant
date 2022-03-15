dbb2b4b3b16672d75f90f041a38621dc131a2858
Jesper Justesen <juj@energinet.dk>
2022-03-15 13:46:10 +0100

    Add new actors to T001 environment

---
+++ source/Energinet.DataHub.MarketParticipant.ApplyDBMigrationsApp/Scripts/T-001/Seed/202203151344 Add Test Actors.sql
@@ -0,0 +1,42 @@
+INSERT [dbo].[ActorInfo] ([Id], [IdentificationNumber], [IdentificationType], [Roles], [Active], [Name])
+VALUES (N'bb71f037-692d-4bcd-9c79-741ad90e33ca', N'8100000000200', 1, N'DDM,MDR,DDQ', 1, N'Titans T-001')
+
+INSERT [dbo].[ActorInfo] ([Id], [IdentificationNumber], [IdentificationType], [Roles], [Active], [Name])
+VALUES (N'e52427c2-204b-4cf2-beec-35b95f935619', N'8100000000201', 1, N'DDM,MDR,DDQ', 1, N'Batman T-001')
+
+INSERT [dbo].[ActorInfo] ([Id], [IdentificationNumber], [IdentificationType], [Roles], [Active], [Name])
+VALUES (N'950af11e-85dd-49da-943f-491c7730ff7a', N'8100000000202', 1, N'DDM,MDR', 1, N'endk-DDM8')
+
+INSERT [dbo].[ActorInfo] ([Id], [IdentificationNumber], [IdentificationType], [Roles], [Active], [Name])
+VALUES (N'5ed12387-2926-4a93-a76c-e779d2b54f4a', N'8100000000203', 1, N'DDM,MDR', 1, N'MD T-001')
+
+INSERT [dbo].[ActorInfo] ([Id], [IdentificationNumber], [IdentificationType], [Roles], [Active], [Name])
+VALUES (N'c2059ddf-ad04-4a99-bc98-2c6fde857025', N'8100000000204', 1, N'DDM,MDR', 1, N'Joules T-001')
+
+INSERT [dbo].[ActorInfo] ([Id], [IdentificationNumber], [IdentificationType], [Roles], [Active], [Name])
+VALUES (N'a6e88928-58c3-458e-92bc-3f2a56dbd09a', N'8100000000205', 1, N'DDM,MDR', 1, N'endk-DDM2')
+
+INSERT [dbo].[ActorInfo] ([Id], [IdentificationNumber], [IdentificationType], [Roles], [Active], [Name])
+VALUES (N'0a05bb66-42b8-4b71-affb-9a3716a94489', N'8100000000206', 1, N'DDM,MDR', 1, N'endk-DDM5')
+
+INSERT [dbo].[ActorInfo] ([Id], [IdentificationNumber], [IdentificationType], [Roles], [Active], [Name])
+VALUES (N'ef69b130-1ead-4e58-a926-a2b1154d7e2a', N'8100000000207', 1, N'DDM,MDR', 1, N'Volt T-001')
+
+INSERT [dbo].[ActorInfo] ([Id], [IdentificationNumber], [IdentificationType], [Roles], [Active], [Name])
+VALUES (N'893ab294-59c1-4c81-add7-0ae7ef80912c', N'8100000000208', 1, N'DDM,MDR', 1, N'endk-DDM3')
+
+INSERT [dbo].[ActorInfo] ([Id], [IdentificationNumber], [IdentificationType], [Roles], [Active], [Name])
+VALUES (N'31e83b3a-22d3-49ce-bdf1-2d456205f0d1', N'8100000000209', 1, N'DDM,MDR', 1, N'endk-DDM6')
+
+INSERT [dbo].[ActorInfo] ([Id], [IdentificationNumber], [IdentificationType], [Roles], [Active], [Name])
+VALUES (N'2ab0e215-537f-4de1-b609-09b9777e31dd', N'8100000000210', 1, N'EZ', 1, N'endk-TSO')
+
+INSERT [dbo].[ActorInfo] ([Id], [IdentificationNumber], [IdentificationType], [Roles], [Active], [Name])
+VALUES (N'5806501c-d376-4859-8376-e2886ce36b90', N'8100000000211', 1, N'DDM,MDR', 1, N'endk-DDM1')
+
+INSERT [dbo].[ActorInfo] ([Id], [IdentificationNumber], [IdentificationType], [Roles], [Active], [Name])
+VALUES (N'55878bd4-ef25-4f2e-8ba8-b2f3cca962e6', N'8100000000212', 1, N'DDM,MDR', 1, N'endk-DDM7')
+
+INSERT [dbo].[ActorInfo] ([Id], [IdentificationNumber], [IdentificationType], [Roles], [Active], [Name])
+VALUES (N'd2eabcd3-3ed1-4dea-a5be-f043b703dac0', N'8100000000213', 1, N'DDM,MDR', 1, N'endk-DDM4')
+GO
