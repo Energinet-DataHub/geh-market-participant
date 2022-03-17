03c4ec99112dd18af308ea188565907d4405818b
Jesper Justesen <juj@energinet.dk>
2022-03-17 14:09:38 +0100

    Fix wrong gridareaId

--- source/Energinet.DataHub.MarketParticipant.ApplyDBMigrationsApp/Scripts/U-001/Seed/202203171403 FixBatmanActors.sql
+++ source/Energinet.DataHub.MarketParticipant.ApplyDBMigrationsApp/Scripts/U-001/Seed/202203171403 FixBatmanActors.sql
@@ -5,7 +5,7 @@ GO
 
 UPDATE [GridAreaInfo]
 SET ActorId = '55878bd4-ef25-4f2e-8ba8-b2f3cca962e6'
-WHERE Id = 'cddf2d12-3f02-4a52-b1c1-c0d4e03262c4'
+WHERE Id = '31e25acd-7a93-4740-b5c6-e931785d5d38'
 GO
 
 UPDATE [GridAreaInfo]
