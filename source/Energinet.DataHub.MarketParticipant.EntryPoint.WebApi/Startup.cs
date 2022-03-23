67015ff7c479c14dadeb08b010b18d8fe93e821a
Jesper Justesen <juj@energinet.dk>
2022-03-23 14:19:22 +0100

    Missed some commented line

--- source/Energinet.DataHub.MarketParticipant.EntryPoint.WebApi/Startup.cs
+++ source/Energinet.DataHub.MarketParticipant.EntryPoint.WebApi/Startup.cs
@@ -116,7 +116,7 @@ namespace Energinet.DataHub.MarketParticipant.EntryPoint.WebApi
             var audience = Configuration["FRONTEND_SERVICE_APP_ID"] ?? throw new InvalidOperationException(
                 "Frontend service app id not found.");
 
-            //Container.AddJwtTokenSecurity(openIdUrl, audience);
+            Container.AddJwtTokenSecurity(openIdUrl, audience);
         }
     }
 }
