edade844c6c28e4e9566b90a7aa9ef83c12ec730
Jesper Justesen <juj@energinet.dk>
2022-03-25 08:37:41 +0100

    Added new test for multiple orgs

--- source/Energinet.DataHub.MarketParticipant.Libraries.Tests/Clients/MarketParticipantClientTests.cs
+++ source/Energinet.DataHub.MarketParticipant.Libraries.Tests/Clients/MarketParticipantClientTests.cs
@@ -20,6 +20,7 @@ using System.Threading.Tasks;
 using Energinet.DataHub.MarketParticipant.Client;
 using Energinet.DataHub.MarketParticipant.Client.Models;
 using Energinet.DataHub.MarketParticipant.Libraries.Tests.Common;
+using Flurl.Http;
 using Flurl.Http.Configuration;
 using Flurl.Http.Testing;
 using Xunit;
@@ -30,22 +31,24 @@ namespace Energinet.DataHub.MarketParticipant.Libraries.Tests.Clients
     [UnitTest]
     public sealed class MarketParticipantClientTests
     {
-        // [Fact]
-        // public async Task GetOrganizationsAsync_Unauthorized_ThrowsException()
-        // {
-        //     // Arrange
-        //     using var messageHandler = new MarketParticipantClientFactory(HttpStatusCode.Unauthorized);
-        //     using var httpClient = messageHandler.CreateHttpClient();
-        //
-        //     var target = new MarketParticipantClient(httpClient);
-        //
-        //     // Act + Assert
-        //     await Assert
-        //         .ThrowsAsync<HttpRequestException>(() => target.GetOrganizationsAsync())
-        //         .ConfigureAwait(false);
-        // }
         [Fact]
-        public async Task GetOrganizationsAsync_All_ReturnsOrganizations()
+        public async Task GetOrganizationsAsync_Unauthorized_ThrowsException()
+        {
+            // Arrange
+            using var httpTest = new HttpTest();
+            using var clientFactory = new PerBaseUrlFlurlClientFactory();
+            var target = new MarketParticipantClient(clientFactory.Get("https://localhost"));
+            httpTest.RespondWith("unauthorized", (int)HttpStatusCode.Unauthorized);
+
+            // Act + Assert
+            var exception = await Assert
+                .ThrowsAsync<FlurlHttpException>(() => target.GetOrganizationsAsync())
+                .ConfigureAwait(false);
+            Assert.Equal(exception.StatusCode, (int)HttpStatusCode.Unauthorized);
+        }
+
+        [Fact]
+        public async Task GetOrganizationsAsync_All_ReturnsOrganization()
         {
             // Arrange
             const string incomingJson = @"
@@ -93,6 +96,92 @@ namespace Energinet.DataHub.MarketParticipant.Libraries.Tests.Clients
 
             var actualMarketRole = actualActor.MarketRoles.Single();
             Assert.Equal(EicFunction.Consumer, actualMarketRole.Function);
+        }
+
+        [Fact]
+        public async Task GetOrganizationsAsync_All_Returns2Organizations()
+        {
+            // Arrange
+            const string incomingJson = @"
+{
+    ""Organizations"": [
+        {
+            ""OrganizationId"": ""fb6665a1-b7be-4744-a8ce-08da0272c916"",
+            ""Name"": ""unit test"",
+            ""Actors"": [
+                {
+                    ""ActorId"": ""8a46b5ac-4c7d-48c0-3f16-08da0279759b"",
+                    ""ExternalActorId"": ""75ea715f-381e-46fd-831b-5b61b9db7862"",
+                    ""Gln"": {
+                        ""Value"": ""9656626091925""
+                    },
+                    ""Status"": ""Active"",
+                    ""MarketRoles"": [
+                        {
+                            ""Function"": ""Consumer""
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            ""OrganizationId"": ""c4d950f7-0acf-439b-9bb6-610255218c6e"",
+            ""Name"": ""unit test 2"",
+            ""Actors"": [
+                {
+                    ""ActorId"": ""f6792b0b-7dee-4e70-b9d9-46b727e6748b"",
+                    ""ExternalActorId"": ""dfef92e2-923e-43aa-8706-ac7445cddfb3"",
+                    ""Gln"": {
+                        ""Value"": ""8574664796620""
+                    },
+                    ""Status"": ""New"",
+                    ""MarketRoles"": [
+                        {
+                            ""Function"": ""Producer""
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+}";
+            using var httpTest = new HttpTest();
+            using var clientFactory = new PerBaseUrlFlurlClientFactory();
+            var target = new MarketParticipantClient(clientFactory.Get("https://localhost"));
+            httpTest.RespondWith(incomingJson);
+
+            // Act
+            var actual = await target.GetOrganizationsAsync().ConfigureAwait(false);
+
+            // Assert
+            Assert.NotNull(actual);
+            actual = actual.ToList();
+            Assert.Equal(2, actual.Count());
+            var firstOrganization = actual.First();
+            Assert.Equal(Guid.Parse("fb6665a1-b7be-4744-a8ce-08da0272c916"), firstOrganization.OrganizationId);
+            Assert.Equal("unit test", firstOrganization.Name);
+
+            var firstActor = firstOrganization.Actors.Single();
+            Assert.Equal(Guid.Parse("8a46b5ac-4c7d-48c0-3f16-08da0279759b"), firstActor.ActorId);
+            Assert.Equal(Guid.Parse("75ea715f-381e-46fd-831b-5b61b9db7862"), firstActor.ExternalActorId);
+            Assert.Equal("9656626091925", firstActor.Gln.Value);
+            Assert.Equal(ActorStatus.Active, firstActor.Status);
+
+            var firstMarketRole = firstActor.MarketRoles.Single();
+            Assert.Equal(EicFunction.Consumer, firstMarketRole.Function);
+
+            var secondOrganization = actual.Skip(1).First();
+            Assert.Equal(Guid.Parse("c4d950f7-0acf-439b-9bb6-610255218c6e"), secondOrganization.OrganizationId);
+            Assert.Equal("unit test 2", secondOrganization.Name);
+
+            var secondActor = secondOrganization.Actors.Single();
+            Assert.Equal(Guid.Parse("f6792b0b-7dee-4e70-b9d9-46b727e6748b"), secondActor.ActorId);
+            Assert.Equal(Guid.Parse("dfef92e2-923e-43aa-8706-ac7445cddfb3"), secondActor.ExternalActorId);
+            Assert.Equal("8574664796620", secondActor.Gln.Value);
+            Assert.Equal(ActorStatus.New, secondActor.Status);
+
+            var secondMarketRole = secondActor.MarketRoles.Single();
+            Assert.Equal(EicFunction.Producer, secondMarketRole.Function);
         }
     }
 }
