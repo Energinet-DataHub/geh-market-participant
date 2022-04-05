91ff6148c67f24f4fedacd1ebf13526bf6fa6e31
Jesper Justesen <juj@energinet.dk>
2022-04-05 13:49:28 +0200

    Changed script

--- source/Energinet.DataHub.MarketParticipant.ApplyDBMigrationsApp/Scripts/U-001/Model/202204051318 Alter Address Columns for Organization.sql
+++ source/Energinet.DataHub.MarketParticipant.ApplyDBMigrationsApp/Scripts/U-001/Model/202204051318 Alter Address Columns for Organization.sql
@@ -1,34 +1,14 @@
-DECLARE @ConstraintName nvarchar(200)
-SELECT @ConstraintName = Name FROM SYS.DEFAULT_CONSTRAINTS
-WHERE PARENT_OBJECT_ID = OBJECT_ID('OrganizationInfo')
-  AND PARENT_COLUMN_ID = (SELECT column_id FROM sys.columns
-                          WHERE NAME = N'BusinessRegisterIdentifier'
-                            AND object_id = OBJECT_ID(N'OrganizationInfo'))
-IF @ConstraintName IS NOT NULL
-    EXEC('ALTER TABLE OrganizationInfo DROP CONSTRAINT ' + @ConstraintName)
-GO
-
-DECLARE @ConstraintName nvarchar(200)
-SELECT @ConstraintName = Name FROM SYS.DEFAULT_CONSTRAINTS
-WHERE PARENT_OBJECT_ID = OBJECT_ID('OrganizationInfo')
-  AND PARENT_COLUMN_ID = (SELECT column_id FROM sys.columns
-                          WHERE NAME = N'Address_Country'
-                            AND object_id = OBJECT_ID(N'OrganizationInfo'))
-IF @ConstraintName IS NOT NULL
-    EXEC('ALTER TABLE OrganizationInfo DROP CONSTRAINT ' + @ConstraintName)
-
-ALTER TABLE [dbo].[OrganizationInfo]
-    ADD CONSTRAINT DF_BusiVal DEFAULT N'' FOR BusinessRegisterIdentifier
-ALTER TABLE [dbo].[OrganizationInfo]
-    ALTER COLUMN BusinessRegisterIdentifier nvarchar(8) NOT NULL
-ALTER TABLE [dbo].[OrganizationInfo]
-    ADD CONSTRAINT DFCountryVal DEFAULT N'' FOR Address_Country
+UPDATE [dbo].[OrganizationInfo]
+SET Address_Country = N'DK'
 GO
 
 UPDATE [dbo].[OrganizationInfo]
-SET Address_Country = N''
+SET BusinessRegisterIdentifier = N''
 GO
 
 ALTER TABLE [dbo].[OrganizationInfo]
     ALTER COLUMN Address_Country nvarchar(50) NOT NULL
+
+ALTER TABLE [dbo].[OrganizationInfo]
+    ALTER COLUMN BusinessRegisterIdentifier nvarchar(8) NOT NULL
 GO
