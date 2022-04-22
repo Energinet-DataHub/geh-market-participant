7c7edd0b80ea7f1b975b483b5d588b3e0c4bdbfb
KarstenM <xnetsrak@gmail.com>
2022-04-22 12:52:56 +0200

    adding a shared parser class.

---
+++ source/Energinet.DataHub.MarketParticipant.Integration.Model/source/Energinet.DataHub.MarketParticipant.Integration.Model/Parsers/SharedIntegrationEventParser.cs
@@ -0,0 +1,102 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using Energinet.DataHub.MarketParticipant.Integration.Model.Dtos;
+using Energinet.DataHub.MarketParticipant.Integration.Model.Exceptions;
+
+namespace Energinet.DataHub.MarketParticipant.Integration.Model.Parsers
+{
+    public class SharedIntegrationEventParser : ISharedIntegrationEventParser
+    {
+        public object Parse(byte[] protoContract)
+        {
+            if (TryParseActorUpdatedIntegrationEvent(protoContract, out ActorUpdatedIntegrationEvent actorUpdatedEvent))
+            {
+                return actorUpdatedEvent;
+            }
+
+            if (TryParseOrganizationUpdatedIntegrationEvent(protoContract, out OrganizationUpdatedIntegrationEvent organizationUpdatedIntegrationEvent))
+            {
+                return organizationUpdatedIntegrationEvent;
+            }
+
+            if (TryParseGridAreaUpdatedIntegrationEvent(protoContract, out GridAreaUpdatedIntegrationEvent gridAreaUpdatedEvent))
+            {
+                return gridAreaUpdatedEvent;
+            }
+
+            throw new MarketParticipantException("IntegrationEventParser not found");
+        }
+
+        private static bool TryParseActorUpdatedIntegrationEvent(
+            byte[] protoContract,
+            out ActorUpdatedIntegrationEvent actorUpdatedEvent)
+        {
+            try
+            {
+                var actorUpdatedEventParser = new ActorUpdatedIntegrationEventParser();
+                var actorEvent = actorUpdatedEventParser.Parse(protoContract);
+                actorUpdatedEvent = actorEvent;
+                return true;
+            }
+#pragma warning disable CA1031
+            catch (Exception)
+#pragma warning restore CA1031
+            {
+                actorUpdatedEvent = null!;
+                return false;
+            }
+        }
+
+        private static bool TryParseGridAreaUpdatedIntegrationEvent(
+            byte[] protoContract,
+            out GridAreaUpdatedIntegrationEvent gridAreaUpdatedIntegrationEvent)
+        {
+            try
+            {
+                var gridAreaUpdatedEventParser = new GridAreaUpdatedIntegrationEventParser();
+                var gridAreaUpdatedEvent = gridAreaUpdatedEventParser.Parse(protoContract);
+                gridAreaUpdatedIntegrationEvent = gridAreaUpdatedEvent;
+                return true;
+            }
+#pragma warning disable CA1031
+            catch (Exception)
+#pragma warning restore CA1031
+            {
+                gridAreaUpdatedIntegrationEvent = null!;
+                return false;
+            }
+        }
+
+        private static bool TryParseOrganizationUpdatedIntegrationEvent(
+            byte[] protoContract,
+            out OrganizationUpdatedIntegrationEvent organizationUpdatedIntegrationEvent)
+        {
+            try
+            {
+                var organizationUpdatedIntegrationEventParser = new OrganizationUpdatedIntegrationEventParser();
+                organizationUpdatedIntegrationEvent = organizationUpdatedIntegrationEventParser.Parse(protoContract);
+                return true;
+            }
+#pragma warning disable CA1031
+            catch (Exception)
+#pragma warning restore CA1031
+            {
+                organizationUpdatedIntegrationEvent = null!;
+                return false;
+            }
+        }
+    }
+}
---
+++ source/Energinet.DataHub.MarketParticipant.Libraries.Tests/Parsers/SharedIntegrationEventParserTests.cs
@@ -0,0 +1,116 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using Energinet.DataHub.MarketParticipant.Integration.Model.Dtos;
+using Energinet.DataHub.MarketParticipant.Integration.Model.Exceptions;
+using Energinet.DataHub.MarketParticipant.Integration.Model.Parsers;
+using Xunit;
+using Xunit.Categories;
+using ActorStatus = Energinet.DataHub.MarketParticipant.Integration.Model.Dtos.ActorStatus;
+using EicFunction = Energinet.DataHub.MarketParticipant.Integration.Model.Dtos.EicFunction;
+
+namespace Energinet.DataHub.MarketParticipant.Libraries.Tests.Parsers
+{
+    [UnitTest]
+    public class SharedIntegrationEventParserTests
+    {
+        [Fact]
+        public void ParseCorrectlyWith_ActorUpdatedIntegrationEventParser()
+        {
+            // arrange
+            var input = new ActorUpdatedIntegrationEventParser();
+            var findAndParse = new SharedIntegrationEventParser();
+
+            var @event = new ActorUpdatedIntegrationEvent(
+                Guid.NewGuid(),
+                Guid.NewGuid(),
+                Guid.NewGuid(),
+                Guid.NewGuid(),
+                "0123456789012",
+                ActorStatus.Active,
+                new[] { BusinessRoleCode.Ddk, BusinessRoleCode.Ddm },
+                new[] { EicFunction.Agent, EicFunction.BalanceResponsibleParty },
+                new[] { Guid.NewGuid(), Guid.NewGuid() });
+
+            // act
+            var actualBytes = input.Parse(@event);
+            var actualEventObject = findAndParse.Parse(actualBytes);
+
+            // assert
+            Assert.IsType<ActorUpdatedIntegrationEvent>(actualEventObject);
+        }
+
+        [Fact]
+        public void ParseCorrectlyWith_GridAreaUpdatedIntegrationEventParser()
+        {
+            // Arrange
+            var input = new GridAreaUpdatedIntegrationEventParser();
+            var findAndParse = new SharedIntegrationEventParser();
+
+            var @event = new GridAreaUpdatedIntegrationEvent(
+                Guid.NewGuid(),
+                Guid.NewGuid(),
+                "TestArea",
+                "123",
+                PriceAreaCode.DK1);
+
+            // Act
+            var actualBytes = input.Parse(@event);
+            var actualEventObject = findAndParse.Parse(actualBytes);
+
+            // Assert
+            Assert.IsType<GridAreaUpdatedIntegrationEvent>(actualEventObject);
+        }
+
+        [Fact]
+        public void ParseCorrectlyWith_OrganizationUpdatedIntegrationEventParser()
+        {
+            // Arrange
+            var input = new OrganizationUpdatedIntegrationEventParser();
+            var findAndParse = new SharedIntegrationEventParser();
+
+            var @event = new OrganizationUpdatedIntegrationEvent(
+                Guid.NewGuid(),
+                Guid.NewGuid(),
+                "TestOrg",
+                "12345678",
+                new Address(
+                    "fake_value",
+                    "fake_value",
+                    "fake_value",
+                    "fake_value",
+                    "fake_value"));
+
+            // Act
+            var actualBytes = input.Parse(@event);
+            var actualEventObject = findAndParse.Parse(actualBytes);
+
+            // Assert
+            Assert.IsType<OrganizationUpdatedIntegrationEvent>(actualEventObject);
+        }
+
+        [Fact]
+        public void ParseException_FallThrough()
+        {
+            // Arrange
+            var findAndParse = new SharedIntegrationEventParser();
+
+            var actualBytes = System.Text.Encoding.ASCII.GetBytes("unknown");
+
+            // Act + Assert
+            Assert.Throws<MarketParticipantException>(() => findAndParse.Parse(actualBytes));
+        }
+    }
+}
