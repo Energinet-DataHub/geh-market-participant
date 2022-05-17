2d4284ad2e23711140d67ff166444e2f97dfa729
Jesper Justesen <juj@energinet.dk>
2022-05-17 08:20:52 +0200

    Update 202205161321 ChangeEllevActors.sql

--- source/Energinet.DataHub.MarketParticipant.ApplyDBMigrationsApp/Scripts/T-001/Seed/202205161321 ChangeEllevActors.sql
+++ source/Energinet.DataHub.MarketParticipant.ApplyDBMigrationsApp/Scripts/T-001/Seed/202205161321 ChangeEllevActors.sql
@@ -4,4 +4,4 @@ WHERE Id = '0e223e42-bed4-4778-a973-8d0ad9813f71'
 
 UPDATE ActorInfo
 SET Id = 'c2d52cac-c5db-4bf4-8fe2-bb76abc04959',
-    WHERE Id = '09591f53-b531-4db5-bb66-a2f52a393d82'
+WHERE Id = '09591f53-b531-4db5-bb66-a2f52a393d82'
