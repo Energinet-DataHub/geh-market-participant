295b92cbd396aefc6fc9c8d92dfb149f92390764
René Nielsen <renetdev@gmail.com>
2022-06-09 08:51:53 +0200

    added keyvault references

--- build/infrastructure/main/app-webapi.tf
+++ build/infrastructure/main/app-webapi.tf
@@ -34,9 +34,9 @@ module "app_webapi" {
     FRONTEND_OPEN_ID_URL                       = "${data.azurerm_key_vault_secret.frontend_open_id_url.value}"
     FRONTEND_SERVICE_APP_ID                    = "${data.azurerm_key_vault_secret.frontend_service_app_id.value}"
     SQL_MP_DB_CONNECTION_STRING                = local.MS_MARKET_PARTICIPANT_CONNECTION_STRING
-    SERVICE_BUS_CONNECTION_STRING              = "${data.azurerm_key_vault_secret.sb_domain_relay_send_connection_string.value}"
-    SERVICE_BUS_HEALTH_CHECK_CONNECTION_STRING = "${data.azurerm_key_vault_secret.sb_domain_relay_manage_connection_string.value}"
-    SBT_MARKET_PARTICIPANT_CHANGED_NAME        = "${data.azurerm_key_vault_secret.sbt-market-participant-changed-name.value}"
+    SERVICE_BUS_CONNECTION_STRING              = "@Microsoft.KeyVault(VaultName=${var.shared_resources_keyvault_name};SecretName=sb-domain-relay-send-connection-string)",
+    SERVICE_BUS_HEALTH_CHECK_CONNECTION_STRING = "@Microsoft.KeyVault(VaultName=${var.shared_resources_keyvault_name};SecretName=sb-domain-relay-manage-connection-string)",
+    SBT_MARKET_PARTICIPANT_CHANGED_NAME        = "@Microsoft.KeyVault(VaultName=${var.shared_resources_keyvault_name};SecretName=sbt-market-participant-changed-name)",
     AZURE_B2C_TENANT                           = var.b2c_tenant
     AZURE_B2C_SPN_ID                           = var.b2c_spn_id
     AZURE_B2C_SPN_SECRET                       = var.b2c_spn_secret
--- build/infrastructure/main/kv-shared-resources.tf
+++ build/infrastructure/main/kv-shared-resources.tf
@@ -41,21 +41,6 @@ data "azurerm_key_vault_secret" "mssql_data_name" {
   key_vault_id = data.azurerm_key_vault.kv_shared_resources.id
 }
 
-data "azurerm_key_vault_secret" "sb_domain_relay_send_connection_string" {
-  name         = "sb-domain-relay-send-connection-string"
-  key_vault_id = data.azurerm_key_vault.kv_shared_resources.id
-}
-
-data "azurerm_key_vault_secret" "sb_domain_relay_manage_connection_string" {
-  name         = "sb-domain-relay-manage-connection-string"
-  key_vault_id = data.azurerm_key_vault.kv_shared_resources.id
-}
-
-data "azurerm_key_vault_secret" "sbt-market-participant-changed-name" {
-  name         = "sbt-market-participant-changed-name"
-  key_vault_id = data.azurerm_key_vault.kv_shared_resources.id
-}
-
 data "azurerm_key_vault_secret" "plan_shared_id" {
   name         = "plan-shared-id"
   key_vault_id = data.azurerm_key_vault.kv_shared_resources.id
