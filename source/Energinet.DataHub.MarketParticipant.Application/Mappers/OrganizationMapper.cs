8a8f051c112846e61202c7afd158026ad2278443
KarstenM <xnetsrak@gmail.com>
2022-06-29 11:19:28 +0200

    adding missing dto mapping

--- source/Energinet.DataHub.MarketParticipant.Application/Mappers/OrganizationMapper.cs
+++ source/Energinet.DataHub.MarketParticipant.Application/Mappers/OrganizationMapper.cs
@@ -60,7 +60,7 @@ namespace Energinet.DataHub.MarketParticipant.Application.Mappers
         {
             return new ActorMarketRoleDto(
                 marketRole.Function.ToString(),
-                marketRole.GridAreas.Select(e => new ActorGridAreaDto(e.Id, new List<string>())));
+                marketRole.GridAreas.Select(e => new ActorGridAreaDto(e.Id, e.MeteringPointTypes.Select(m => m.Name))));
         }
     }
 }
