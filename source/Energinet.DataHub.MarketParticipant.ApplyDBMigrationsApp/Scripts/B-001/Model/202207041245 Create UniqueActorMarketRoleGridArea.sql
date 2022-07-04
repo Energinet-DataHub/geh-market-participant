e14e1759a344eeb0f57c9fe306afe8d718be2e46
michaelmajgaard <52096428+michaelmajgaard@users.noreply.github.com>
2022-07-04 13:18:07 +0200

    fixed typo

--- source/Energinet.DataHub.MarketParticipant.ApplyDBMigrationsApp/Scripts/B-001/Model/202207041245 Create UniqueActorMarketRoleGridArea.sql
+++ source/Energinet.DataHub.MarketParticipant.ApplyDBMigrationsApp/Scripts/B-001/Model/202207041245 Create UniqueActorMarketRoleGridArea.sql
@@ -8,6 +8,6 @@ CREATE TABLE [dbo].[UniqueActorMarketRoleGridArea]
     CONSTRAINT PK_UniqueActorMarketRoleGridArea PRIMARY KEY ([Id]),
     CONSTRAINT FK_UniqueActorMarketRoleGridArea_Actor FOREIGN KEY ([ActorId]) REFERENCES [dbo].[ActorInfoNew]([Id]),
     CONSTRAINT FK_UniqueActorMarketRoleGridArea_GridArea FOREIGN KEY ([GridAreaId]) REFERENCES [dbo].[GridAreaNew]([Id]),
-    CONSTRAINT UQ_UniqueActorMarketRoleGridArea_MarketROleFunction_GridAreaId UNIQUE ([MarketRoleFunction], [GridAreaId])
+    CONSTRAINT UQ_UniqueActorMarketRoleGridArea_MarketRoleFunction_GridAreaId UNIQUE ([MarketRoleFunction], [GridAreaId])
 )
 GO
