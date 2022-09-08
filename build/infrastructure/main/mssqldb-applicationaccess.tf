6835d5afae23317e867d2dc4a566051259b94e13
René Nielsen <renetdev@gmail.com>
2022-09-08 08:32:35 +0200

    use elastic pool

--- build/infrastructure/main/mssqldb-applicationaccess.tf
+++ build/infrastructure/main/mssqldb-applicationaccess.tf
@@ -17,12 +17,12 @@ module "mssql_database_application_access" {
   sql_server_name         = data.azurerm_mssql_server.mssqlsrv.name
   database_name           = module.mssqldb_market_participant.name
   application_hosts_names = [
-                              module.app_webapi.name,
-                              module.func_entrypoint_marketparticipant.name,
-                            ]
+    module.app_webapi.name,
+    module.func_entrypoint_marketparticipant.name,
+  ]
 
   depends_on              = [
-                              module.app_webapi.name,
-                              module.func_entrypoint_marketparticipant.name,
-                            ]
+    module.app_webapi.name,
+    module.func_entrypoint_marketparticipant.name,
+  ]
 }
