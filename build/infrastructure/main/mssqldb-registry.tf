e22be1ac6257941685dec07e57c2ab99bc390598
René Nielsen <renetdev@gmail.com>
2022-09-08 10:51:22 +0200

    fixes

--- build/infrastructure/main/mssqldb-registry.tf
+++ build/infrastructure/main/mssqldb-registry.tf
@@ -17,7 +17,7 @@ data "azurerm_mssql_server" "mssqlsrv" {
 }
 
 module "mssqldb_market_participant" {
-  source                      = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/mssql-database?ref=7.1.0"
+  source                      = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/mssql-database?ref=v9"
 
   name                        = "registry"
   project_name                = var.domain_name_short
