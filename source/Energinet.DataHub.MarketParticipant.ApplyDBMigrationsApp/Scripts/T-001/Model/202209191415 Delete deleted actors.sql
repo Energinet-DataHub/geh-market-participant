80ddc439cbd4980739cce152fc1d9c95cb275d32
michaelmajgaard <52096428+michaelmajgaard@users.noreply.github.com>
2022-09-19 14:48:56 +0200

    deleted actors script change

--- source/Energinet.DataHub.MarketParticipant.ApplyDBMigrationsApp/Scripts/T-001/Model/202209191415 Delete deleted actors.sql
+++ source/Energinet.DataHub.MarketParticipant.ApplyDBMigrationsApp/Scripts/T-001/Model/202209191415 Delete deleted actors.sql
@@ -3,16 +3,8 @@ FROM [dbo].[ActorContact] x
 JOIN [dbo].[ActorInfoNew] a ON x.ActorId = a.Id AND a.Status = 5
 
 DELETE x
-FROM [dbo].[ActorInfoMeteringType] x 
-JOIN [dbo].[ActorInfoNew] a ON x.ActorInfoId = a.Id AND a.Status = 5
-
-DELETE x
 FROM [dbo].[DomainEvent] x 
 JOIN [dbo].[ActorInfoNew] a ON x.EntityId = a.Id AND a.Status = 5
-
-DELETE x
-FROM [dbo].[GridAreaActorInfoLink] x 
-JOIN [dbo].[ActorInfoNew] a ON x.ActorInfoId = a.Id AND a.Status = 5
 
 DELETE z
 FROM [dbo].[GridAreaMeteringPointType] z
