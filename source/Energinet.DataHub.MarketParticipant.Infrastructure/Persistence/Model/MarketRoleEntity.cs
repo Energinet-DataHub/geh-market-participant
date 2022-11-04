f043ce05021e5620bd6405101e035e30bc8fcfbc
Morten Vestergaard Hansen <mimo@mimo-design.com>
2022-11-04 11:50:14 +0100

    Added missing setter

--- source/Energinet.DataHub.MarketParticipant.Infrastructure/Persistence/Model/MarketRoleEntity.cs
+++ source/Energinet.DataHub.MarketParticipant.Infrastructure/Persistence/Model/MarketRoleEntity.cs
@@ -29,6 +29,6 @@ namespace Energinet.DataHub.MarketParticipant.Infrastructure.Persistence.Model
         public Guid ActorInfoId { get; set; }
         public EicFunction Function { get; set; }
         public Collection<MarketRoleGridAreaEntity> GridAreas { get; }
-        public string? Comment { get; }
+        public string? Comment { get; set; }
     }
 }
