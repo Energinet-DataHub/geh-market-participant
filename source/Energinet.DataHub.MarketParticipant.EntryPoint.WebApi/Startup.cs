6894847b62fb5a00d30469c0869ee97feb72c5f6
Alexei Mihalchuk <AIU@energinet.dk>
2022-11-25 14:19:03 +0100

    refac: Update UTs to work with DB.

--- source/Energinet.DataHub.MarketParticipant.EntryPoint.WebApi/Startup.cs
+++ source/Energinet.DataHub.MarketParticipant.EntryPoint.WebApi/Startup.cs
@@ -124,8 +124,6 @@ namespace Energinet.DataHub.MarketParticipant.EntryPoint.WebApi
             var serviceBusConnectionString = configuration.GetSetting(Settings.ServiceBusHealthCheckConnectionString);
             var serviceBusTopicName = configuration.GetSetting(Settings.ServiceBusTopicName);
 
-            services.AddApplicationInsightsTelemetry();
-
             // Health check
             services
                 .AddHealthChecks()
--- source/Energinet.DataHub.MarketParticipant.IntegrationTests/Common/MockedGln.cs
+++ source/Energinet.DataHub.MarketParticipant.IntegrationTests/Common/MockedGln.cs
@@ -19,87 +19,86 @@ using Energinet.DataHub.MarketParticipant.Application.Commands.Actor;
 using Energinet.DataHub.MarketParticipant.Domain.Model;
 using Energinet.DataHub.MarketParticipant.Domain.Model.IntegrationEvents.ActorIntegrationEvents;
 
-namespace Energinet.DataHub.MarketParticipant.IntegrationTests.Common
+namespace Energinet.DataHub.MarketParticipant.IntegrationTests.Common;
+
+public sealed class MockedGln
 {
-    public sealed class MockedGln
-    {
-        private static readonly Random _rng = new();
-        private readonly string _gln = CreateRandomGln();
+    private static readonly Random _rng = new();
+    private readonly string _gln = CreateRandomGln();
 
 #pragma warning disable CA1062, CA2225, CA5394
-        public static implicit operator ActorNumber(MockedGln mock)
-        {
-            return ActorNumber.Create(mock._gln);
-        }
+    public static implicit operator ActorNumber(MockedGln mock)
+    {
+        return ActorNumber.Create(mock._gln);
+    }
 
-        public static implicit operator ActorNumberEventData(MockedGln mock)
-        {
-            return new ActorNumberEventData(mock._gln, ActorNumberType.Gln);
-        }
+    public static implicit operator ActorNumberEventData(MockedGln mock)
+    {
+        return new ActorNumberEventData(mock._gln, ActorNumberType.Gln);
+    }
 
-        public static implicit operator ActorNumberDto(MockedGln mock)
-        {
-            return new ActorNumberDto(mock._gln);
-        }
+    public static implicit operator ActorNumberDto(MockedGln mock)
+    {
+        return new ActorNumberDto(mock._gln);
+    }
 
-        public static implicit operator string(MockedGln mock)
-        {
-            return mock._gln;
-        }
+    public static implicit operator string(MockedGln mock)
+    {
+        return mock._gln;
+    }
 
-        public override string ToString()
-        {
-            return _gln;
-        }
+    public override string ToString()
+    {
+        return _gln;
+    }
 
-        private static IEnumerable<int> GetRandomDigits()
+    private static IEnumerable<int> GetRandomDigits()
+    {
+        for (var i = 0; i < 12; i++)
         {
-            for (var i = 0; i < 12; i++)
-            {
-                yield return _rng.Next(0, 10);
-            }
+            yield return _rng.Next(0, 10);
         }
+    }
 #pragma warning restore
 
-        private static string CreateRandomGln()
+    private static string CreateRandomGln()
+    {
+        string location;
+
+        lock (_rng)
         {
-            string location;
-
-            lock (_rng)
-            {
-                location = string.Concat(GetRandomDigits());
-            }
-
-            for (var checkDigit = 0; checkDigit < 10; checkDigit++)
-            {
-                var gln = location + checkDigit;
-
-                if (CalculateChecksum(gln) == checkDigit)
-                    return gln;
-            }
-
-            return string.Empty;
+            location = string.Concat(GetRandomDigits());
         }
 
-        private static int CalculateChecksum(string glnNumber)
+        for (var checkDigit = 0; checkDigit < 10; checkDigit++)
         {
-            var sumOfOddNumbers = 0;
-            var sumOfEvenNumbers = 0;
+            var gln = location + checkDigit;
 
-            for (var i = 1; i < glnNumber.Length; i++)
-            {
-                var currentNumber = int.Parse(glnNumber[(i - 1)..i], CultureInfo.InvariantCulture);
-
-                if (i % 2 == 0)
-                    sumOfEvenNumbers += currentNumber;
-                else
-                    sumOfOddNumbers += currentNumber;
-            }
-
-            var sum = (sumOfEvenNumbers * 3) + sumOfOddNumbers;
-
-            var equalOrHigherMultipleOf = (int)(Math.Ceiling(sum / 10.0) * 10);
-            return equalOrHigherMultipleOf - sum;
+            if (CalculateChecksum(gln) == checkDigit)
+                return gln;
         }
+
+        return string.Empty;
+    }
+
+    private static int CalculateChecksum(string glnNumber)
+    {
+        var sumOfOddNumbers = 0;
+        var sumOfEvenNumbers = 0;
+
+        for (var i = 1; i < glnNumber.Length; i++)
+        {
+            var currentNumber = int.Parse(glnNumber[(i - 1)..i], CultureInfo.InvariantCulture);
+
+            if (i % 2 == 0)
+                sumOfEvenNumbers += currentNumber;
+            else
+                sumOfOddNumbers += currentNumber;
+        }
+
+        var sum = (sumOfEvenNumbers * 3) + sumOfOddNumbers;
+
+        var equalOrHigherMultipleOf = (int)(Math.Ceiling(sum / 10.0) * 10);
+        return equalOrHigherMultipleOf - sum;
     }
 }
