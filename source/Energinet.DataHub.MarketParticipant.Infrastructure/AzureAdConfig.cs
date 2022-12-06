f9acc9b0b69a0be53b7990125d84a10cde347c37
Alexei Mihalchuk <AIU@energinet.dk>
2022-12-06 16:00:10 +0100

    Trigger CI.

--- source/Energinet.DataHub.MarketParticipant.Infrastructure/AzureAdConfig.cs
+++ source/Energinet.DataHub.MarketParticipant.Infrastructure/AzureAdConfig.cs
@@ -12,9 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-namespace Energinet.DataHub.MarketParticipant.Infrastructure
-{
-    public sealed record AzureAdConfig(
-        string BackendAppServicePrincipalObjectId,
-        string BackendAppId);
-}
+namespace Energinet.DataHub.MarketParticipant.Infrastructure;
+
+public sealed record AzureAdConfig(
+    string BackendAppServicePrincipalObjectId,
+    string BackendAppId);
