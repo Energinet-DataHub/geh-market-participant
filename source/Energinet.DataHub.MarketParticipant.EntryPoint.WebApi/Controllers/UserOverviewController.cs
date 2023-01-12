774b91f6becf2fe829e3663f9db9b5472aa7522a
Alexei Mihalchuk <AIU@energinet.dk>
2023-01-12 13:49:21 +0100

    bug: Fix unsupported query.

--- source/Energinet.DataHub.MarketParticipant.EntryPoint.WebApi/Controllers/UserOverviewController.cs
+++ source/Energinet.DataHub.MarketParticipant.EntryPoint.WebApi/Controllers/UserOverviewController.cs
@@ -45,7 +45,7 @@ public sealed class UserOverviewController : ControllerBase
 
     [HttpGet("users")]
     [AuthorizeUser(Permission.UsersManage)]
-    public async Task<IActionResult> GetUserOverviewAsync(int pageNumber, int pageSize, string? searchText, IEnumerable<UserStatus> userStatus)
+    public async Task<IActionResult> GetUserOverviewAsync(int pageNumber, int pageSize, string? searchText, UserStatus[]? userStatus)
     {
         return await this.ProcessAsync(
             async () =>
@@ -57,7 +57,7 @@ public sealed class UserOverviewController : ControllerBase
                 var filter = new UserOverviewFilterDto(
                     actorId,
                     searchText,
-                    userStatus);
+                    userStatus ?? Array.Empty<UserStatus>());
 
                 var command = new GetUserOverviewCommand(filter, pageNumber, pageSize);
                 var response = await _mediator.Send(command).ConfigureAwait(false);
