dc235c3d573150d77d9deaab478315d15ae07b9c
Jesper Justesen <juj@energinet.dk>
2023-01-23 21:40:13 +0100

    Added GetSelectablePermissions Handler

---
+++ source/Energinet.DataHub.MarketParticipant.Application/Handlers/UserRoles/GetSelectablePermissionsHandler.cs
@@ -0,0 +1,42 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Energinet.DataHub.Core.App.Common.Security;
+using Energinet.DataHub.MarketParticipant.Application.Commands.UserRoles;
+using Energinet.DataHub.MarketParticipant.Domain.Exception;
+using Energinet.DataHub.MarketParticipant.Domain.Model.Users;
+using Energinet.DataHub.MarketParticipant.Domain.Repositories;
+using MediatR;
+
+namespace Energinet.DataHub.MarketParticipant.Application.Handlers.UserRoles;
+
+public sealed class GetSelectablePermissionsHandler
+    : IRequestHandler<GetSelectablePermissionsCommand, GetSelectablePermissionsResponse>
+{
+    public Task<GetSelectablePermissionsResponse> Handle(
+        GetSelectablePermissionsCommand request,
+        CancellationToken cancellationToken)
+    {
+        ArgumentNullException.ThrowIfNull(request);
+        return Task.FromResult(new GetSelectablePermissionsResponse(Enum.GetValues<Permission>().Select(permission =>
+            new SelectablePermissionDto(
+                permission.ToString(),
+                permission.ToString(),
+                permission.ToString()))));
+    }
+}
