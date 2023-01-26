8e23355073a08ca0eec73c4b2213c218ec0a25d2
Alexei Mihalchuk <AIU@energinet.dk>
2023-01-26 14:34:29 +0100

    cleanup: Remove tables for actor v1.

---
+++ source/Energinet.DataHub.MarketParticipant.ApplyDBMigrationsApp/Scripts/LocalDB/202301261415 Remove old actor tables.sql
@@ -0,0 +1,7 @@
+DROP VIEW [dbo].[GridAreaLink];
+DROP VIEW [dbo].[GridArea];
+DROP VIEW [dbo].[Actor];
+DROP TABLE [dbo].[GridAreaLinkInfo];
+DROP TABLE [dbo].[GridAreaInfo];
+DROP TABLE [dbo].[ActorInfo];
+GO
