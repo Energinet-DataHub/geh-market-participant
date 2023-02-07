e167503566ad5fb643716f5ee37b6d9492517806
Jesper Justesen <juj@energinet.dk>
2023-02-07 08:47:35 +0100

    Added missing DI registration

--- source/Energinet.DataHub.MarketParticipant.Common/ApplicationServiceRegistration.cs
+++ source/Energinet.DataHub.MarketParticipant.Common/ApplicationServiceRegistration.cs
@@ -74,6 +74,7 @@ namespace Energinet.DataHub.MarketParticipant.Common
             container.Register<IValidator<GetSelectionActorsQueryCommand>, GetSelectionActorsQueryCommandRuleSet>();
             container.Register<IValidator<SynchronizeActorsCommand>, SynchronizeActorsCommandRuleSet>();
             container.Register<IValidator<GetSelectablePermissionsCommand>, GetSelectablePermissionsCommandRuleSet>();
+            container.Register<IUserRoleHelperService, UserRoleHelperService>();
 
             container.Register<IActiveDirectoryB2CService, ActiveDirectoryB2cService>(Lifestyle.Scoped);
             container.Register<IOrganizationExistsHelperService, OrganizationExistsHelperService>(Lifestyle.Scoped);
