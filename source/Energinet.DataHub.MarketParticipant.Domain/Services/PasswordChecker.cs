d0e74e1b04fa627011190f6aafccf047a591f6ef
michaelmajgaard <52096428+michaelmajgaard@users.noreply.github.com>
2023-02-15 11:15:55 +0100

    clean up

--- source/Energinet.DataHub.MarketParticipant.Domain/Services/PasswordChecker.cs
+++ source/Energinet.DataHub.MarketParticipant.Domain/Services/PasswordChecker.cs
@@ -37,9 +37,9 @@ public sealed class PasswordChecker : IPasswordChecker
             throw new InvalidOperationException($"{nameof(minLength)} must be greather than 0");
         }
 
-        var charSets = _sets.Where(x => (characterSets & x.Key) == x.Key).Select(x => x.Value).ToArray();
+        var charSets = _sets.Where(x => characterSets.HasFlag(x.Key)).Select(x => x.Value).ToList();
 
-        if (minNumberOfSetsToHit < 0 || minNumberOfSetsToHit > charSets.Length)
+        if (minNumberOfSetsToHit < 0 || minNumberOfSetsToHit > charSets.Count)
         {
             throw new InvalidOperationException($"{nameof(minNumberOfSetsToHit)} must be greater than or equal to 0 and less than or equal to the number of {nameof(characterSets)} specified.");
         }
@@ -49,13 +49,13 @@ public sealed class PasswordChecker : IPasswordChecker
             return false;
         }
 
-        var hits = new bool[charSets.Length];
+        var hits = new bool[charSets.Count];
 
         foreach (var c in password)
         {
             var hit = false;
 
-            for (var i = 0; i < charSets.Length; ++i)
+            for (var i = 0; i < charSets.Count; ++i)
             {
                 if (charSets[i].Contains(c))
                 {
