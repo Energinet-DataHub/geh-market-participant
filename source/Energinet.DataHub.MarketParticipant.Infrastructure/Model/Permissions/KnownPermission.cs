60470ecdb5b7032e928a94e89305995c6fd7bd1e
Alex <77341673+defectiveAi@users.noreply.github.com>
2023-03-15 15:19:12 +0100

    Update docs and add tests.

--- source/Energinet.DataHub.MarketParticipant.Infrastructure/Model/Permissions/KnownPermission.cs
+++ source/Energinet.DataHub.MarketParticipant.Infrastructure/Model/Permissions/KnownPermission.cs
@@ -21,6 +21,13 @@ using NodaTime;
 
 namespace Energinet.DataHub.MarketParticipant.Infrastructure.Model.Permissions;
 
+/// <summary>
+/// Represents a known permission. All permissions must be registered in <see cref="KnownPermissions"/> class.
+/// </summary>
+/// <param name="Id">The id of the permission.</param>
+/// <param name="Claim">The claim to be used for the given permission. The claim will be sent with the token and shown to the users in the UI. Use a simple and concise value; the convention is [kebab-cased-plural-feature-name]:[access], e.g. grid-areas:manage or organizations:view.</param>
+/// <param name="Created">The date for when the permission has been introduced. Used for audit logs only.</param>
+/// <param name="AssignableTo">A list of market roles that may support the permission. If a given actor or a given user role does not belong to any of the market roles on this list, the permission will not apply to them.</param>
 public sealed record KnownPermission(
     PermissionId Id,
     string Claim,
---
+++ source/Energinet.DataHub.MarketParticipant.Tests/Model/KnownPermissionsTests.cs
@@ -0,0 +1,53 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Linq;
+using Energinet.DataHub.MarketParticipant.Infrastructure.Model.Permissions;
+using NodaTime;
+using Xunit;
+using Xunit.Categories;
+
+namespace Energinet.DataHub.MarketParticipant.Tests.Model;
+
+[UnitTest]
+public sealed class KnownPermissionsTests
+{
+    [Fact]
+    public void All_Permissions_AreUnique()
+    {
+        // Arrange + Act + Assert
+        Assert.Equal(KnownPermissions.All.Count, KnownPermissions.All.Select(p => p.Id).Distinct().Count());
+    }
+
+    [Fact]
+    public void All_Claims_AreUnique()
+    {
+        // Arrange + Act + Assert
+        Assert.Equal(KnownPermissions.All.Count, KnownPermissions.All.Select(p => p.Claim).Distinct().Count());
+    }
+
+    [Fact]
+    public void All_Created_AreInThePast()
+    {
+        // Arrange + Act + Assert
+        Assert.True(KnownPermissions.All.All(p => p.Created < SystemClock.Instance.GetCurrentInstant()));
+    }
+
+    [Fact]
+    public void All_AssignableTo_HaveItems()
+    {
+        // Arrange + Act + Assert
+        Assert.True(KnownPermissions.All.All(p => p.AssignableTo.Any()));
+    }
+}
