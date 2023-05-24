c3b03ff50ad4f2e28fc1c95634612ac9d27c7b8a
KarstenM <xnetsrak@gmail.com>
2023-05-24 15:27:42 +0200

    user update

--- source/marketparticipant/Energinet.DataHub.MarketParticipant.Domain/Model/Users/User.cs
+++ source/marketparticipant/Energinet.DataHub.MarketParticipant.Domain/Model/Users/User.cs
@@ -66,7 +66,7 @@ public sealed class User
 
     public void ValidateLogonRequirements()
     {
-        if (InvitationExpiresAt.HasValue && InvitationExpiresAt < DateTimeOffset.UtcNow)
+        if (InvitationExpiresAt < DateTimeOffset.UtcNow)
         {
             throw new UnauthorizedAccessException("User invitation has expired");
         }
