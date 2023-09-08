2751111d5fff117c02b0c90fab7450e5e51101d9
Jesper Justesen <juj@energinet.dk>
2023-09-08 08:07:21 +0200

    Removed TestSQL

--- source/marketparticipant/Energinet.DataHub.MarketParticipant.ApplyDBMigrationsApp/Scripts/LocalDB/202308291500 Add LittleBobbyTables.sql
+++
@@ -1,7 +0,0 @@
-IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[LittleBobbyTables]') AND type IN (N'U'))
-BEGIN
-    CREATE TABLE [dbo].[LittleBobbyTables]
-    (
-        [Id] [int] IDENTITY(1, 1) NOT NULL,
-    )
-END
