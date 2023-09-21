3e0f47eda982e06d9a62e1fe81e938e1bfe4bcf9
Alex <77341673+defectiveAi@users.noreply.github.com>
2023-09-21 15:34:59 +0200

    Patch Energinet Bootstrap for d-001.

---
+++ source/marketparticipant/Energinet.DataHub.MarketParticipant.ApplyDBMigrationsApp/Scripts/D-001/Seed/202309211530 Patch Energinet Bootstrap.sql
@@ -0,0 +1,7 @@
+UPDATE [dbo].[Actor]
+SET [ActorId] = '<TODO>'
+WHERE [ActorId] = '23b17b7f-2927-4714-8a88-2a26291c3921';
+
+UPDATE [dbo].[User]
+SET [ExternalId] = '<TODO>'
+WHERE [ExternalId] = '21f352ee-7308-4bfe-b9bf-ad8a3cb362ae';
