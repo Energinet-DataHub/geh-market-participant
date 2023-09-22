b43c6dd9b4051322b749dc7271833ae634277552
Alex <77341673+defectiveAi@users.noreply.github.com>
2023-09-22 13:21:19 +0200

    Update 202309211530 Patch Energinet Bootstrap.sql

--- source/marketparticipant/Energinet.DataHub.MarketParticipant.ApplyDBMigrationsApp/Scripts/D-001/Seed/202309211530 Patch Energinet Bootstrap.sql
+++ source/marketparticipant/Energinet.DataHub.MarketParticipant.ApplyDBMigrationsApp/Scripts/D-001/Seed/202309211530 Patch Energinet Bootstrap.sql
@@ -1,7 +1,7 @@
 UPDATE [dbo].[Actor]
-SET [ActorId] = '<TODO>'
+SET [ActorId] = 'c7de79dc-b465-4933-af70-84db97c07bf4'
 WHERE [ActorId] = '23b17b7f-2927-4714-8a88-2a26291c3921';
 
 UPDATE [dbo].[User]
-SET [ExternalId] = '<TODO>'
+SET [ExternalId] = 'e0da02cd-9e84-47e8-a1bf-d26826ad1a82'
 WHERE [ExternalId] = '21f352ee-7308-4bfe-b9bf-ad8a3cb362ae';
