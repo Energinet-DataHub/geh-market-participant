af312bf9910517ca41075dfc8572e5436a0fcfa3
Alex <77341673+defectiveAi@users.noreply.github.com>
2023-09-25 14:58:26 +0200

    Add audit history to Permission.

---
+++ source/marketparticipant/Energinet.DataHub.MarketParticipant.ApplyDBMigrationsApp/Scripts/LocalDB/202309251400 Add Permission History.sql
@@ -0,0 +1,24 @@
+ALTER TABLE [dbo].[Permission] ADD
+
+    PeriodStart DATETIME2 GENERATED ALWAYS AS ROW START HIDDEN
+    CONSTRAINT DF_Permission_PeriodStart DEFAULT SYSUTCDATETIME(),
+    PeriodEnd DATETIME2 GENERATED ALWAYS AS ROW END HIDDEN
+    CONSTRAINT DF_Permission_PeriodEnd DEFAULT CONVERT(DATETIME2, '9999-12-31 23:59:59.9999999'),
+    PERIOD FOR SYSTEM_TIME(PeriodStart, PeriodEnd),
+    
+    Version             int              NOT NULL DEFAULT 0,
+    ChangedByIdentityId uniqueidentifier NOT NULL
+	CONSTRAINT DF_ChangedByIdentityId DEFAULT('00000000-FFFF-FFFF-FFFF-000000000000');
+GO
+
+ALTER TABLE [dbo].[Permission]
+    SET (SYSTEM_VERSIONING = ON (HISTORY_TABLE = dbo.[PermissionHistory]));
+GO
+
+ALTER TABLE [dbo].[Permission]
+    DROP CONSTRAINT DF_ChangedByIdentityId
+GO
+
+ALTER TABLE [dbo].[Permission]
+    ADD CONSTRAINT CHK_Permission_ChangedByIdentityId_NotEmpty CHECK (ChangedByIdentityId <> '00000000-0000-0000-0000-000000000000');
+GO
--- source/marketparticipant/Energinet.DataHub.MarketParticipant.Infrastructure/Extensions/AuditExtensions.cs
+++ source/marketparticipant/Energinet.DataHub.MarketParticipant.Infrastructure/Extensions/AuditExtensions.cs
@@ -24,7 +24,7 @@ using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace Energinet.DataHub.MarketParticipant.Infrastructure.Extensions;
 
-public static class HistoryAuditExtensions
+public static class AuditExtensions
 {
     public static async Task<IReadOnlyList<(T Entity, DateTimeOffset PeriodStart)>> ReadAllHistoryForAsync<T>(this DbSet<T> target, Expression<Func<T, bool>> entitySelector)
         where T : class, IAuditedEntity
