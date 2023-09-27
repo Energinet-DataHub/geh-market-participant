2e3716c7e4a8d6cd0cdfccbf18299153abae470b
KarstenM <xnetsrak@gmail.com>
2023-09-27 08:59:46 +0200

    removed UserRoleAuditLogService.cs

--- source/marketparticipant/Energinet.DataHub.MarketParticipant.Common/ApplicationServiceRegistration.cs
+++ source/marketparticipant/Energinet.DataHub.MarketParticipant.Common/ApplicationServiceRegistration.cs
@@ -87,7 +87,6 @@ namespace Energinet.DataHub.MarketParticipant.Common
 
             services.AddScoped<IActiveDirectoryB2CService, ActiveDirectoryB2CService>();
             services.AddScoped<IOrganizationExistsHelperService, OrganizationExistsHelperService>();
-            services.AddScoped<IUserRoleAuditLogService, UserRoleAuditLogService>();
             services.AddScoped<IExternalActorSynchronizationRepository, ExternalActorSynchronizationRepository>();
             services.AddScoped<IUserIdentityOpenIdLinkService, UserIdentityOpenIdLinkService>();
             services.AddScoped<IAuditIdentityResolver, AuditIdentityResolver>();
