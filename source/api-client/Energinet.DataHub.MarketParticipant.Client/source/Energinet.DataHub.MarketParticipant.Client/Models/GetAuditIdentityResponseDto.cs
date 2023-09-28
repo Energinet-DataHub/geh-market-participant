06d5bf930969aada2fc195ea4c3e9f927ecd8e9f
Alex <77341673+defectiveAi@users.noreply.github.com>
2023-09-28 15:06:34 +0200

    Update BFF package for audit identity.

--- source/api-client/Energinet.DataHub.MarketParticipant.Client/source/Energinet.DataHub.MarketParticipant.Client/Models/GetAuditIdentityResponseDto.cs
+++ source/api-client/Energinet.DataHub.MarketParticipant.Client/source/Energinet.DataHub.MarketParticipant.Client/Models/GetAuditIdentityResponseDto.cs
@@ -12,15 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using System;
-
 namespace Energinet.DataHub.MarketParticipant.Client.Models
 {
-    public sealed record GridAreaAuditLogEntryDto(
-        DateTimeOffset Timestamp,
-        string OldValue,
-        string NewValue,
-        Guid GridAreaId,
-        Guid AuditIdentityId,
-        GridAreaAuditLogEntryField Field);
+    public sealed record GetAuditIdentityResponseDto(string DisplayName);
 }
