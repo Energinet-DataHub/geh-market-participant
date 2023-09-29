1679805db2bc41920b4ba72cf1e4817e411bd5f8
KarstenM <xnetsrak@gmail.com>
2023-09-29 12:29:59 +0200

    db scripts added and changed

--- source/marketparticipant/Energinet.DataHub.MarketParticipant.ApplyDBMigrationsApp/Scripts/T-001/Model/202309121230 Add UserRole History.sql
+++ source/marketparticipant/Energinet.DataHub.MarketParticipant.ApplyDBMigrationsApp/Scripts/T-001/Model/202309121230 Add UserRole History.sql
@@ -1,14 +1,14 @@
 /* UserRole Temporal table setup */
 ALTER TABLE [dbo].[UserRole]
     ADD
-    PeriodStart DATETIME2 GENERATED ALWAYS AS ROW START HIDDEN
-    CONSTRAINT DF_UserRole_PeriodStart DEFAULT SYSUTCDATETIME(),
-    PeriodEnd DATETIME2 GENERATED ALWAYS AS ROW END HIDDEN
-    CONSTRAINT DF_UserRole_PeriodEnd DEFAULT CONVERT(DATETIME2, '9999-12-31 23:59:59.9999999'),
-    PERIOD FOR SYSTEM_TIME(PeriodStart, PeriodEnd),
-    Version int NOT NULL DEFAULT 0,
-    ChangedByIdentityId uniqueidentifier NOT NULL
-	CONSTRAINT DF_ChangedByIdentityId DEFAULT('00000000-FFFF-FFFF-FFFF-000000000000');
+        PeriodStart DATETIME2 GENERATED ALWAYS AS ROW START HIDDEN
+            CONSTRAINT DF_UserRole_PeriodStart DEFAULT SYSUTCDATETIME(),
+        PeriodEnd DATETIME2 GENERATED ALWAYS AS ROW END HIDDEN
+            CONSTRAINT DF_UserRole_PeriodEnd DEFAULT CONVERT(DATETIME2, '9999-12-31 23:59:59.9999999'),
+        PERIOD FOR SYSTEM_TIME(PeriodStart, PeriodEnd),
+        Version int NOT NULL DEFAULT 0,
+        ChangedByIdentityId uniqueidentifier NOT NULL
+            CONSTRAINT DF_ChangedByIdentityId DEFAULT('00000000-FFFF-FFFF-FFFF-000000000000');
 GO
 
 ALTER TABLE [dbo].[UserRole]
@@ -25,14 +25,15 @@ GO
 /* UserRolePermission Temporal table setup */
 ALTER TABLE [dbo].[UserRolePermission]
     ADD
-    PeriodStart DATETIME2 GENERATED ALWAYS AS ROW START HIDDEN
-    CONSTRAINT DF_UserRolePermission_PeriodStart DEFAULT SYSUTCDATETIME(),
-    PeriodEnd DATETIME2 GENERATED ALWAYS AS ROW END HIDDEN
-    CONSTRAINT DF_UserRolePermission_PeriodEnd DEFAULT CONVERT(DATETIME2, '9999-12-31 23:59:59.9999999'),
-    PERIOD FOR SYSTEM_TIME(PeriodStart, PeriodEnd),
-	ChangedByIdentityId uniqueidentifier NOT NULL
-	CONSTRAINT DF_ChangedByIdentityId DEFAULT('00000000-FFFF-FFFF-FFFF-000000000000'),
-    DeletedByIdentityId uniqueidentifier NULL                                   
+        PeriodStart DATETIME2 GENERATED ALWAYS AS ROW START HIDDEN
+            CONSTRAINT DF_UserRolePermission_PeriodStart DEFAULT SYSUTCDATETIME(),
+        PeriodEnd DATETIME2 GENERATED ALWAYS AS ROW END HIDDEN
+            CONSTRAINT DF_UserRolePermission_PeriodEnd DEFAULT CONVERT(DATETIME2, '9999-12-31 23:59:59.9999999'),
+        PERIOD FOR SYSTEM_TIME(PeriodStart, PeriodEnd),
+        Version int NOT NULL DEFAULT 0,
+        ChangedByIdentityId uniqueidentifier NOT NULL
+            CONSTRAINT DF_ChangedByIdentityId DEFAULT('00000000-FFFF-FFFF-FFFF-000000000000'),
+        DeletedByIdentityId uniqueidentifier NULL
 GO
 
 ALTER TABLE [dbo].[UserRolePermission]
@@ -44,4 +45,8 @@ GO
 
 ALTER TABLE [dbo].[UserRolePermission]
     ADD CONSTRAINT CHK_UserRolePermission_ChangedByIdentityId_NotEmpty CHECK (ChangedByIdentityId <> '00000000-0000-0000-0000-000000000000');
+GO
+
+ALTER TABLE [dbo].[UserRolePermission]
+    ADD CONSTRAINT CHK_UserRolePermission_DeletedByIdentityId_NotEmpty CHECK (DeletedByIdentityId <> '00000000-0000-0000-0000-000000000000');
 GO
