ecde5c5122a256e762fa1d723101ea540db3c66a
Michael <52096428+michaelmajgaard@users.noreply.github.com>
2023-10-03 13:37:10 +0200

    chore: add default changed by identity (#631)

--- source/marketparticipant/Energinet.DataHub.MarketParticipant.ApplyDBMigrationsApp/Scripts/DEV-002/Seed/202307061100 Bootstrap Energinet.sql
+++ source/marketparticipant/Energinet.DataHub.MarketParticipant.ApplyDBMigrationsApp/Scripts/DEV-002/Seed/202307061100 Bootstrap Energinet.sql
@@ -6,14 +6,15 @@ INSERT INTO [dbo].[GridArea] (
     [PriceAreaCode],
     [ValidFrom],
     [ValidTo],
-    [FullFlexDate]
+    [FullFlexDate],
+[ChangedByIdentityId]
 )
 VALUES
-    (NEWID(), '533', 'Netområde 533', 1, '2000-01-01', NULL, NULL),
-    (NEWID(), '543', 'Netområde 543', 1, '2000-01-01', NULL, NULL),
-    (NEWID(), '584', 'Netområde 584', 1, '2000-01-01', NULL, NULL),
-    (NEWID(), '803', 'Netområde 803', 1, '2000-01-01', NULL, NULL),
-    (NEWID(), '804', 'Netområde 804', 1, '2000-01-01', NULL, NULL);
+    (NEWID(), '533', 'Netområde 533', 1, '2000-01-01', NULL, NULL, '00000000-FFFF-FFFF-FFFF-000000000000'),
+    (NEWID(), '543', 'Netområde 543', 1, '2000-01-01', NULL, NULL, '00000000-FFFF-FFFF-FFFF-000000000000'),
+    (NEWID(), '584', 'Netområde 584', 1, '2000-01-01', NULL, NULL, '00000000-FFFF-FFFF-FFFF-000000000000'),
+    (NEWID(), '803', 'Netområde 803', 1, '2000-01-01', NULL, NULL, '00000000-FFFF-FFFF-FFFF-000000000000'),
+    (NEWID(), '804', 'Netområde 804', 1, '2000-01-01', NULL, NULL, '00000000-FFFF-FFFF-FFFF-000000000000');
 
 -- Create Energinet Organisation
 DECLARE @organisationId varchar(36) = '10000000-0000-0000-0000-000000000000'
@@ -61,7 +62,7 @@ VALUES
     (
         @actorId,
         @organisationId,
-        '39e8ef5a-f492-47fa-bf82-4aa39bdc641c',
+        '23b17b7f-2927-4714-8a88-2a26291c3921',
         '5790001330583',
         2,
         'Energinet DataHub A/S (DataHub systemadministrator)',
@@ -69,10 +70,10 @@ VALUES
 
 -- Assign Market Role
 INSERT INTO [dbo].[MarketRole] (
-	[Id],
+    [Id],
     [ActorId],
     [Function],
-    [Comment]
+[Comment]
 )
 VALUES
     (NEWID(), @actorId, 50, NULL);
@@ -92,7 +93,7 @@ INSERT INTO [dbo].[User] (
 VALUES
     (
         @userId,
-        '3ce71fbd-89ad-428d-bd0e-a69291f54e58',
+        '21f352ee-7308-4bfe-b9bf-ad8a3cb362ae',
         'bootstrap.datahub@energinet.dk',
         NULL,
         '00000000-0000-1111-0000-000000000000',
@@ -105,17 +106,19 @@ DECLARE @userAdminRoleId varchar(36) = 'f3df856f-bd11-4174-97cb-fb6bc54c300a'
 INSERT INTO [dbo].[UserRoleAssignment] (
     [UserId],
     [ActorId],
-    [UserRoleId]
+    [UserRoleId],
+    [ChangedByIdentityId]
 )
 VALUES
-    (@userId, @actorId, @userAdminRoleId);
+    (@userId, @actorId, @userAdminRoleId, '00000000-FFFF-FFFF-FFFF-000000000000');
 
 INSERT INTO [dbo].[UserRolePermission] (
     [UserRoleId],
-    [PermissionId]
+    [PermissionId],
+[ChangedByIdentityId]
 )
 VALUES
-    (@userAdminRoleId, 5),
-    (@userAdminRoleId, 6),
-    (@userAdminRoleId, 7),
-    (@userAdminRoleId, 8);
+    (@userAdminRoleId, 5, '00000000-FFFF-FFFF-FFFF-000000000000'),
+    (@userAdminRoleId, 6, '00000000-FFFF-FFFF-FFFF-000000000000'),
+    (@userAdminRoleId, 7, '00000000-FFFF-FFFF-FFFF-000000000000'),
+    (@userAdminRoleId, 8, '00000000-FFFF-FFFF-FFFF-000000000000');
