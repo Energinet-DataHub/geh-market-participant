388b523986b98edee79c82fe79f62e0d0a1fe72c
Nicolaj Aalykke Hansen <nicolaj.it@icloud.com>
2023-10-03 10:03:33 +0200

    Bootstrap test_001 user (#628)
    * added bootstrap file for test_001

---
+++ source/marketparticipant/Energinet.DataHub.MarketParticipant.ApplyDBMigrationsApp/Scripts/TEST-001/Seed/202310031530 Patch Energinet Bootstrap.sql
@@ -0,0 +1,7 @@
+UPDATE [dbo].[Actor]
+SET [ActorId] = '49086290-ff2c-423e-8502-713f9633892f'
+WHERE [ActorId] = '23b17b7f-2927-4714-8a88-2a26291c3921';
+
+UPDATE [dbo].[User]
+SET [ExternalId] = '6542be40-be54-4f92-83ab-651276d9532e'
+WHERE [ExternalId] = '21f352ee-7308-4bfe-b9bf-ad8a3cb362ae';
