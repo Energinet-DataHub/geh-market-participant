4fb8da68211a5ba2674c69e339b7e7cca7a9610f
Jesper Justesen <1972142+FirestarJes@users.noreply.github.com>
2023-10-13 15:45:10 +0200

    Fix wrong script

--- source/marketparticipant/Energinet.DataHub.MarketParticipant.ApplyDBMigrationsApp/Scripts/LocalDB/202310130927 Add Organization History.sql
+++ source/marketparticipant/Energinet.DataHub.MarketParticipant.ApplyDBMigrationsApp/Scripts/LocalDB/202310130927 Add Organization History.sql
@@ -1,9 +1,9 @@
-ALTER TABLE [dbo].[GridArea] ADD
+ALTER TABLE [dbo].[Organization] ADD
 
     PeriodStart DATETIME2 GENERATED ALWAYS AS ROW START HIDDEN
-    CONSTRAINT DF_GridArea_PeriodStart DEFAULT SYSUTCDATETIME(),
-    PeriodEnd   DATETIME2 GENERATED ALWAYS AS ROW END HIDDEN
-    CONSTRAINT DF_GridArea_PeriodEnd   DEFAULT CONVERT(DATETIME2, '9999-12-31 23:59:59.9999999'),
+    CONSTRAINT DF_Organization_PeriodStart DEFAULT SYSUTCDATETIME(),
+    PeriodEnd DATETIME2 GENERATED ALWAYS AS ROW END HIDDEN
+    CONSTRAINT DF_Organization_PeriodEnd   DEFAULT CONVERT(DATETIME2, '9999-12-31 23:59:59.9999999'),
     PERIOD FOR SYSTEM_TIME(PeriodStart, PeriodEnd),
     
     Version             int              NOT NULL DEFAULT 0,
@@ -11,14 +11,14 @@ ALTER TABLE [dbo].[GridArea] ADD
     CONSTRAINT DF_ChangedByIdentityId DEFAULT('00000000-FFFF-FFFF-FFFF-000000000000');
 GO
 
-ALTER TABLE [dbo].[GridArea]
-    SET (SYSTEM_VERSIONING = ON (HISTORY_TABLE = dbo.[GridAreaHistory]));
+ALTER TABLE [dbo].[Organization]
+    SET (SYSTEM_VERSIONING = ON (HISTORY_TABLE = dbo.[Organization]));
 GO
 
-ALTER TABLE [dbo].[GridArea]
-    DROP CONSTRAINT DF_ChangedByIdentityId
+ALTER TABLE [dbo].[Organization]
+DROP CONSTRAINT DF_ChangedByIdentityId
 GO
 
-ALTER TABLE [dbo].[GridArea]
-    ADD CONSTRAINT CHK_GridArea_ChangedByIdentityId_NotEmpty CHECK (ChangedByIdentityId <> '00000000-0000-0000-0000-000000000000');
+ALTER TABLE [dbo].[Organization]
+    ADD CONSTRAINT CHK_Organization_ChangedByIdentityId_NotEmpty CHECK (ChangedByIdentityId <> '00000000-0000-0000-0000-000000000000');
 GO
