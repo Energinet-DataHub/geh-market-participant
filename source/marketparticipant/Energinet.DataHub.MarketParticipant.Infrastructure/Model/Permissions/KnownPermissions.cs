fa5b22e1db3fb4044fd18b7125a98b7836ff4324
Alex <77341673+defectiveAi@users.noreply.github.com>
2023-10-25 13:12:52 +0200

    Add BalanceResponsibleParty to settlement-reports permission.

--- source/marketparticipant/Energinet.DataHub.MarketParticipant.Infrastructure/Model/Permissions/KnownPermissions.cs
+++ source/marketparticipant/Energinet.DataHub.MarketParticipant.Infrastructure/Model/Permissions/KnownPermissions.cs
@@ -86,6 +86,7 @@ public static class KnownPermissions
             EicFunction.EnergySupplier,
             EicFunction.GridAccessProvider,
             EicFunction.MeteredDataResponsible,
+            EicFunction.BalanceResponsibleParty,
             EicFunction.DataHubAdministrator
         }),
         new(PermissionId.ESettExchangeManage, "esett-exchange:manage", InstantPattern.ExtendedIso.Parse("2023-09-01T00:00:00Z").Value, new[]
