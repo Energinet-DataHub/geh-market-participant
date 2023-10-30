e4d40dfdb81039feb9e461557120cb3dc6a7d68e
Jesper Justesen <jesper.justesen@gmail.com>
2023-10-30 11:15:38 +0100

    Added script files to deploy folder

---
+++ source/marketparticipant/Energinet.DataHub.MarketParticipant.ApplyDBMigrationsApp/Scripts/ALL/Model/202310271143 Add ActorCertificateCredentials Table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE [dbo].[ActorCertificateCredentials]
+(
+    [ActorId]       [uniqueidentifier]   NOT NULL,
+    [Thumbprint]    [nvarchar](40)      NOT NULL,
+    [KvSecretId]    [nvarchar](Max)      NOT NULL
+    CONSTRAINT [UQ_ActorCertificateCredentials_Thumbprint] UNIQUE NONCLUSTERED
+(
+[Thumbprint] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+    ) ON [PRIMARY]
