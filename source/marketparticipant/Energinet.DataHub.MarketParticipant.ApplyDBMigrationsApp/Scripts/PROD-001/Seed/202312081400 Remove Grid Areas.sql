743d2e50b2569df724c3c962a35e8ac12abc5b28
Alex <77341673+defectiveAi@users.noreply.github.com>
2023-12-08 14:20:58 +0100

    What the hell...

--- source/marketparticipant/Energinet.DataHub.MarketParticipant.ApplyDBMigrationsApp/Scripts/PROD-001/Seed/202312081400 Remove Grid Areas.sql
+++ source/marketparticipant/Energinet.DataHub.MarketParticipant.ApplyDBMigrationsApp/Scripts/PROD-001/Seed/202312081400 Remove Grid Areas.sql
@@ -1,1 +1,1 @@
-DELETE FROM [GridAreas] WHERE Name in ('Netområde 533', 'Netområde 543', 'Netområde 584', 'Netområde 803', 'Netområde 804')
+DELETE FROM [GridArea] WHERE Name in ('Netområde 533', 'Netområde 543', 'Netområde 584', 'Netområde 803', 'Netområde 804')
