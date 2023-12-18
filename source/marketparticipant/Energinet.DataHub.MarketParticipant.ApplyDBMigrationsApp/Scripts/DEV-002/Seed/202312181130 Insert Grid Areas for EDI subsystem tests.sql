5e3c03953e6286b85811e6b1e6d4fa1fe61ba733
Mads Magnus Due <madsmagnusdue@hotmail.com>
2023-12-18 13:17:10 +0100

    added to test001 as well

--- source/marketparticipant/Energinet.DataHub.MarketParticipant.ApplyDBMigrationsApp/Scripts/DEV-002/Seed/202312181130 Insert Grid Areas for EDI subsystem tests.sql
+++ source/marketparticipant/Energinet.DataHub.MarketParticipant.ApplyDBMigrationsApp/Scripts/DEV-002/Seed/202312181130 Insert Grid Areas for EDI subsystem tests.sql
@@ -1,2 +1,2 @@
-INSERT INTO [dbo].[GridArea] ([Id], [Code], [Name], [PriceAreaCode], [ValidFrom], [FullFlexDate], [ChangedByIdentityId])
+﻿INSERT INTO [dbo].[GridArea] ([Id], [Code], [Name], [PriceAreaCode], [ValidFrom], [FullFlexDate], [ChangedByIdentityId])
 VALUES (NEWID(), '543', 'Netområde 543', 1, CONVERT(datetime, '21-02-2018 23:00:00', 105), CONVERT(datetime, '31-12-2019 23:00:00', 105), '00000000-FFFF-FFFF-FFFF-000000000000')
