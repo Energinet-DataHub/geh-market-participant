0394f6f292907407e19250a11113d259c5ab1c0b
KarstenM <xnetsrak@gmail.com>
2024-01-22 14:31:19 +0100

    script update

--- source/marketparticipant/Energinet.DataHub.MarketParticipant.ApplyDBMigrationsApp/Scripts/LocalDB/202401221000 Alter UsedActorCertificates Table.sql
+++ source/marketparticipant/Energinet.DataHub.MarketParticipant.ApplyDBMigrationsApp/Scripts/LocalDB/202401221000 Alter UsedActorCertificates Table.sql
@@ -3,6 +3,10 @@ ALTER TABLE [dbo].[UsedActorCertificates]
     CONSTRAINT [FK_UsedActorCertificates_Actor] FOREIGN KEY ([ActorId]) REFERENCES [dbo].[Actor] ([Id])
 GO
 
+UPDATE [dbo].[UsedActorCertificates] SET [ActorId] = '00000000-0000-0000-0000-000000000000' WHERE [ActorId] IS NULL
+
+GO
+
 UPDATE [dbo].[UsedActorCertificates]
 SET [ActorId] = [dbo].[ActorCertificateCredentials].[ActorId]
 FROM [dbo].[UsedActorCertificates]
