5ebd699ed500514aa00774a8f14ef2f3189182c6
Michael <52096428+michaelmajgaard@users.noreply.github.com>
2024-02-14 14:21:32 +0100

    feat: cvr register - organization identity api endpoint (#789)

---
+++ source/marketparticipant/Energinet.DataHub.MarketParticipant.Application/Handlers/Organization/GetOrganizationIdentityHandler.cs
@@ -0,0 +1,45 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Energinet.DataHub.MarketParticipant.Application.Commands.Organization;
+using Energinet.DataHub.MarketParticipant.Domain.Model;
+using Energinet.DataHub.MarketParticipant.Domain.Repositories;
+using MediatR;
+
+namespace Energinet.DataHub.MarketParticipant.Application.Handlers.Organization;
+
+public sealed class GetOrganizationIdentityHandler : IRequestHandler<GetOrganizationIdentityCommand, GetOrganizationIdentityResponse>
+{
+    private readonly IOrganizationIdentityRepository _organizationIdentityRepository;
+
+    public GetOrganizationIdentityHandler(IOrganizationIdentityRepository organizationIdentityRepository)
+    {
+        _organizationIdentityRepository = organizationIdentityRepository;
+    }
+
+    public async Task<GetOrganizationIdentityResponse> Handle(GetOrganizationIdentityCommand request, CancellationToken cancellationToken)
+    {
+        ArgumentNullException.ThrowIfNull(request);
+
+        var identity = await _organizationIdentityRepository.GetAsync(
+            new BusinessRegisterIdentifier(request.BusinessRegisterIdentifier)).ConfigureAwait(false);
+
+        return identity != null
+            ? new GetOrganizationIdentityResponse(true, new OrganizationIdentityDto(identity.Name))
+            : new GetOrganizationIdentityResponse(false, null);
+    }
+}
---
+++ source/marketparticipant/Energinet.DataHub.MarketParticipant.Tests/Handlers/GetOrganizationIdentityHandlerTests.cs
@@ -0,0 +1,70 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Threading;
+using System.Threading.Tasks;
+using Energinet.DataHub.MarketParticipant.Application.Commands.Organization;
+using Energinet.DataHub.MarketParticipant.Application.Handlers.Organization;
+using Energinet.DataHub.MarketParticipant.Domain.Model;
+using Energinet.DataHub.MarketParticipant.Domain.Repositories;
+using Moq;
+using Xunit;
+using Xunit.Categories;
+
+namespace Energinet.DataHub.MarketParticipant.Tests.Handlers;
+
+[UnitTest]
+public sealed class GetOrganizationIdentityHandlerTests
+{
+    [Fact]
+    public async Task GetOrganizationIdentityHandler_WhenOrganizationIdentityExist_ReturnsFoundOrganizationIdentity()
+    {
+        // arrange
+        var organizationIdentityRepositoryMock = new Mock<IOrganizationIdentityRepository>();
+        organizationIdentityRepositoryMock
+            .Setup(organizationIdentityRepository => organizationIdentityRepository.GetAsync(new BusinessRegisterIdentifier("12345678")))
+            .ReturnsAsync(new OrganizationIdentity("OrganizationName"));
+
+        var target = new GetOrganizationIdentityHandler(organizationIdentityRepositoryMock.Object);
+
+        // act
+        var actual = await target.Handle(new GetOrganizationIdentityCommand("12345678"), CancellationToken.None);
+
+        // assert
+        Assert.NotNull(actual);
+        Assert.True(actual.OrganizationFound);
+        Assert.NotNull(actual.OrganizationIdentity);
+        Assert.Equal("OrganizationName", actual.OrganizationIdentity.Name);
+    }
+
+    [Fact]
+    public async Task GetOrganizationIdentityHandler_WhenOrganizationIdentityDoesNotExist_ReturnsNotFound()
+    {
+        // arrange
+        var organizationIdentityRepositoryMock = new Mock<IOrganizationIdentityRepository>();
+        organizationIdentityRepositoryMock
+            .Setup(organizationIdentityRepository => organizationIdentityRepository.GetAsync(It.IsAny<BusinessRegisterIdentifier>()))
+            .ReturnsAsync((OrganizationIdentity?)null);
+
+        var target = new GetOrganizationIdentityHandler(organizationIdentityRepositoryMock.Object);
+
+        // act
+        var actual = await target.Handle(new GetOrganizationIdentityCommand("12345678"), CancellationToken.None);
+
+        // assert
+        Assert.NotNull(actual);
+        Assert.False(actual.OrganizationFound);
+        Assert.Null(actual.OrganizationIdentity);
+    }
+}
