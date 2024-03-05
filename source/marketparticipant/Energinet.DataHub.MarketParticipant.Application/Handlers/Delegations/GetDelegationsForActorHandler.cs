73927099db412775a4231d974259b35d533ddc70
Jesper Justesen <1972142+FirestarJes@users.noreply.github.com>
2024-03-05 13:59:24 +0100

    Added preliminary model for delegations

---
+++ source/marketparticipant/Energinet.DataHub.MarketParticipant.Application/Handlers/Delegations/GetDelegationsForActorHandler.cs
@@ -0,0 +1,50 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.ObjectModel;
+using System.Threading;
+using System.Threading.Tasks;
+using Energinet.DataHub.MarketParticipant.Application.Commands.Delegations;
+using Energinet.DataHub.MarketParticipant.Domain.Exception;
+using Energinet.DataHub.MarketParticipant.Domain.Model;
+using Energinet.DataHub.MarketParticipant.Domain.Model.Delegations;
+using Energinet.DataHub.MarketParticipant.Domain.Repositories;
+using MediatR;
+
+namespace Energinet.DataHub.MarketParticipant.Application.Handlers.Delegations
+{
+    public sealed class GetDelegationsForActorHandler : IRequestHandler<GetDelegationsForActorCommand, GetDelegationsForActorResponse>
+    {
+        private readonly IActorRepository _actorRepository;
+
+        public GetDelegationsForActorHandler(IActorRepository actorRepository)
+        {
+            _actorRepository = actorRepository;
+        }
+
+        public async Task<GetDelegationsForActorResponse> Handle(GetDelegationsForActorCommand request, CancellationToken cancellationToken)
+        {
+            ArgumentNullException.ThrowIfNull(request, nameof(request));
+
+            var actor = await _actorRepository
+                .GetAsync(new ActorId(request.ActorId))
+                .ConfigureAwait(false);
+
+            NotFoundValidationException.ThrowIfNull(actor, request.ActorId);
+
+            return new GetDelegationsForActorResponse(new Collection<ActorDelegation>());
+        }
+    }
+}
