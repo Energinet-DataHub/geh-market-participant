6b92a153fa04742db6d7a415e87c56928d97443f
Alex <77341673+defectiveAi@users.noreply.github.com>
2024-03-12 11:41:24 +0100

    Update model with skeleton validation.

--- source/marketparticipant/Energinet.DataHub.MarketParticipant.Domain/Model/Delegations/MessageDelegationId.cs
+++ source/marketparticipant/Energinet.DataHub.MarketParticipant.Domain/Model/Delegations/MessageDelegationId.cs
@@ -12,15 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using System;
+
 namespace Energinet.DataHub.MarketParticipant.Domain.Model.Delegations;
 
-public enum DelegationMessageType
-{
-    Rsm012Inbound,
-    Rsm014Inbound,
-    Rsm016Inbound,
-    Rsm016Outbound,
-    Rsm017Inbound,
-    Rsm017Outbound,
-    Rsm019Inbound
-}
+public sealed record MessageDelegationId(Guid Value);
