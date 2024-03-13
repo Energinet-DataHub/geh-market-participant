6f5d8178eca006477f9b08c66d6649757c8090ab
KarstenM <xnetsrak@gmail.com>
2024-03-13 17:03:54 +0100

    integration event test , ignore internal event type

--- source/marketparticipant/Energinet.DataHub.MarketParticipant.IntegrationTests/Services/IntegrationEventProviderIntegrationTests.cs
+++ source/marketparticipant/Energinet.DataHub.MarketParticipant.IntegrationTests/Services/IntegrationEventProviderIntegrationTests.cs
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Energinet.DataHub.Core.Messaging.Communication.Publisher;
@@ -65,10 +66,13 @@ public sealed class IntegrationEventProviderIntegrationTests
         await using var scope = host.BeginScope();
         await using var context = _fixture.DatabaseManager.CreateDbContext();
 
+        var internalDomainEventTypes = new List<string> { nameof(BalanceResponsiblePartiesChanged) };
+
         var allIntegrationEvents = typeof(DomainEvent)
             .Assembly
             .GetTypes()
             .Where(domainEventType => domainEventType.IsSubclassOf(typeof(DomainEvent)))
+            .Where(d => !internalDomainEventTypes.Contains(d.Name))
             .ToDictionary(x => x.Name);
 
         foreach (var (eventName, eventType) in allIntegrationEvents)
