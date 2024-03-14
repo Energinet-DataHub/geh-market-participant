c81cb9d2dd6a3bb8695b5ab9891f8099f3f208aa
Jesper Justesen <1972142+FirestarJes@users.noreply.github.com>
2024-03-14 15:11:02 +0100

    Changed TypedIds in API DTO's to primitive types (#828)

--- source/marketparticipant/Energinet.DataHub.MarketParticipant.Application/Commands/Delegations/MessageDelegationDto.cs
+++ source/marketparticipant/Energinet.DataHub.MarketParticipant.Application/Commands/Delegations/MessageDelegationDto.cs
@@ -12,14 +12,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using System;
 using System.Collections.Generic;
-using Energinet.DataHub.MarketParticipant.Domain.Model;
 using Energinet.DataHub.MarketParticipant.Domain.Model.Delegations;
 
 namespace Energinet.DataHub.MarketParticipant.Application.Commands.Delegations;
 
 public sealed record MessageDelegationDto(
-    MessageDelegationId Id,
-    ActorId DelegatedBy,
+    Guid Id,
+    Guid DelegatedBy,
     DelegationMessageType MessageType,
     IEnumerable<MessageDelegationPeriodDto> Periods);
