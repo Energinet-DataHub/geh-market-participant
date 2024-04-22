0f1d2d38ca2ce580883c166097c54b4a27fa953d
michaelmajgaard <52096428+michaelmajgaard@users.noreply.github.com>
2024-04-22 10:06:07 +0200

    chore: grid area 466 - preprod

---
+++ source/marketparticipant/Energinet.DataHub.MarketParticipant.ApplyDBMigrationsApp/Scripts/PREPROD-001/Seed/202404220915 New GridArea.sql
@@ -0,0 +1,2 @@
+INSERT INTO [dbo].[GridArea] ([Id], [Code], [Name], [PriceAreaCode], [ValidFrom], [FullFlexDate], [ChangedByIdentityId])
+VALUES (NEWID(), '466', 'Net 466', 2, CONVERT(datetime, '01-01-1950 00:00:00', 105), CONVERT(datetime, '01-02-2021 00:00:00', 105), '00000000-FFFF-FFFF-FFFF-000000000000')
