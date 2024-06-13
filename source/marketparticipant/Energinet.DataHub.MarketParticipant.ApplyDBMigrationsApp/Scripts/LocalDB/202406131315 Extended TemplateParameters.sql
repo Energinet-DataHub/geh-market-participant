892fd9e6098bd5ff26d56035775f2857054ccc4c
Alex <77341673+defectiveAi@users.noreply.github.com>
2024-06-13 14:13:32 +0200

    Update SQL.

--- source/marketparticipant/Energinet.DataHub.MarketParticipant.ApplyDBMigrationsApp/Scripts/LocalDB/202406131315 Extended TemplateParameters.sql
+++ source/marketparticipant/Energinet.DataHub.MarketParticipant.ApplyDBMigrationsApp/Scripts/LocalDB/202406131315 Extended TemplateParameters.sql
@@ -1,3 +1,20 @@
+DECLARE @ConstraintName NVARCHAR(255);
+DECLARE @SQL NVARCHAR(MAX);
+
+SELECT 
+    @ConstraintName = df.name
+FROM 
+    sys.default_constraints df
+JOIN 
+    sys.columns clmns ON df.parent_object_id = clmns.object_id AND df.parent_column_id = clmns.column_id
+JOIN 
+    sys.tables tbls ON df.parent_object_id = tbls.object_id
+WHERE 
+    tbls.name = 'EmailEvent' AND clmns.name = 'TemplateParameters';
+
+SET @SQL = 'ALTER TABLE [dbo].[EmailEvent] DROP CONSTRAINT ' + QUOTENAME(@ConstraintName);
+EXEC sp_executesql @SQL;
+
 ALTER TABLE [dbo].[EmailEvent]
     ALTER COLUMN [TemplateParameters] nvarchar(max) NOT NULL
 GO
