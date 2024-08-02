220f038d2ec75299cf01c0ab64842f44579dd684
Alex <77341673+defectiveAi@users.noreply.github.com>
2024-08-02 10:23:43 +0200

    Remove comment.

--- source/marketparticipant/Energinet.DataHub.MarketParticipant.EntryPoint.WebApi/Revision/RevisionLogMiddleware.cs
+++ source/marketparticipant/Energinet.DataHub.MarketParticipant.EntryPoint.WebApi/Revision/RevisionLogMiddleware.cs
@@ -114,7 +114,6 @@ public sealed class RevisionLogMiddleware : IMiddleware
 
     private static Guid GetUserId(IEnumerable<Claim> claims)
     {
-        // The use of 'ClaimTypes.NameIdentifier' is explained here: https://github.com/AzureAD/azure-activedirectory-identitymodel-extensions-for-dotnet/issues/415
         var userId = claims.Single(claim => claim.Type == ClaimTypes.NameIdentifier).Value;
         return Guid.Parse(userId);
     }
