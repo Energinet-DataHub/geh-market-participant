79cb5b44abd6d2758e4eb63b70ae5aedbf499ea9
Jesper Justesen <1972142+FirestarJes@users.noreply.github.com>
2024-08-13 13:33:56 +0200

    Update KnownPermissions.cs

--- source/marketparticipant/Energinet.DataHub.MarketParticipant.Infrastructure/Model/Permissions/KnownPermissions.cs
+++ source/marketparticipant/Energinet.DataHub.MarketParticipant.Infrastructure/Model/Permissions/KnownPermissions.cs
@@ -162,6 +162,10 @@ public static class KnownPermissions
             EicFunction.SystemOperator,
             EicFunction.GridAccessProvider,
         }),
+        new(PermissionId.CalculationsView, "calculations:view", InstantPattern.ExtendedIso.Parse("2024-08-09T00:00:00Z").Value, new[]
+        {
+            EicFunction.DataHubAdministrator
+        }),
         new(PermissionId.ImbalancePricesView, "imbalance-prices:view", InstantPattern.ExtendedIso.Parse("2024-08-09T00:00:00Z").Value, new[]
         {
             EicFunction.BalanceResponsibleParty,
