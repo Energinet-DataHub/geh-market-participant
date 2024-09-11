156e8ac1dce9cf664a04f1ddbc74229d596306bc
Alex <77341673+defectiveAi@users.noreply.github.com>
2024-09-11 08:25:35 +0200

    Upgrade packages.

--- source/marketparticipant/Energinet.DataHub.MarketParticipant.EntryPoint.CertificateSynchronization/Program.cs
+++ source/marketparticipant/Energinet.DataHub.MarketParticipant.EntryPoint.CertificateSynchronization/Program.cs
@@ -21,7 +21,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
-    .ConfigureFunctionsWorkerDefaults()
+    .ConfigureFunctionsWebApplication()
     .ConfigureServices((context, services) =>
     {
         services.AddApplicationInsightsForIsolatedWorker("mark-part");
