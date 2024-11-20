4e24ce1d57c83180444d4e64e27459994f1a59a3
Kristian Bank Erbou <contact@kbeconsult.dk>
2024-11-20 14:42:56 +0100

    Add missing SQL scripts to preprod_002 (#1030)

---
+++ source/marketparticipant/Energinet.DataHub.MarketParticipant.ApplyDBMigrationsApp/Scripts/PREPROD-002/Seed/202405151030 Patch Energinet Bootstrap.sql
@@ -0,0 +1,7 @@
+UPDATE [dbo].[Actor]
+SET [ActorId] = '07c635fa-8bfa-4469-9123-7ffc8fcfe2fd'
+WHERE [ActorId] = '23b17b7f-2927-4714-8a88-2a26291c3921';
+
+UPDATE [dbo].[User]
+SET [ExternalId] = '07055577-7891-48ec-b5f3-6c8b47a944b6'
+WHERE [ExternalId] = '21f352ee-7308-4bfe-b9bf-ad8a3cb362ae';
