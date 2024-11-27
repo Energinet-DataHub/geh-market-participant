1710d05f029b4c192dd1485c990540eefb7371aa
Jesper Justesen <1972142+FirestarJes@users.noreply.github.com>
2024-11-27 10:46:25 +0100

    Added function and handler to consolidate actors

--- source/marketparticipant/Energinet.DataHub.MarketParticipant.Domain/Repositories/IActorConsolidationRepository.cs
+++ source/marketparticipant/Energinet.DataHub.MarketParticipant.Domain/Repositories/IActorConsolidationRepository.cs
@@ -43,5 +43,11 @@ public interface IActorConsolidationRepository
     /// </summary>
     /// <param name="id">The <see cref="ActorId">id</see> of the <see cref="Actor">Actor</see> you want to get consolidations for.</param>
     /// <returns>A List of <see cref="ActorConsolidation">consolidations</see>; or empty list if none found.</returns>
-    Task<IEnumerable<ActorConsolidation>>? GetByActorIdAsync(ActorId id);
+    Task<IEnumerable<ActorConsolidation>> GetByActorIdAsync(ActorId id);
+
+    /// <summary>
+    /// Gets a List of <see cref="ActorConsolidation"/> that are ready to be consolidated.
+    /// </summary>
+    /// <returns>A <see cref="IEnumerable{ActorConsolidation}"/> that are ready to be consolidated.</returns>
+    Task<IEnumerable<ActorConsolidation>> GetReadyToConsolidateAsync();
 }
