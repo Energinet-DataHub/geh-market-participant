e4d535ecb43f5fa0174ce276610d62729587de4d
Jesper Justesen <1972142+FirestarJes@users.noreply.github.com>
2024-11-28 07:40:30 +0100

    Started implementation of Notification

---
+++ source/marketparticipant/Energinet.DataHub.MarketParticipant.Domain/Model/Events/ActorConsolidationExecuted.cs
@@ -0,0 +1,45 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.ComponentModel;
+using System.Text.Json.Serialization;
+
+namespace Energinet.DataHub.MarketParticipant.Domain.Model.Events;
+
+public sealed class ActorConsolidationExecuted : NotificationEvent
+{
+    [JsonConstructor]
+    [Browsable(false)]
+    public ActorConsolidationExecuted(
+        Guid eventId,
+        ActorId recipient,
+        ActorId affectedActorId)
+        : base(recipient)
+    {
+        EventId = eventId;
+        AffectedActorId = affectedActorId;
+    }
+
+    public ActorConsolidationExecuted(
+        ActorId recipient,
+        ActorId affectedActorId)
+        : base(recipient)
+    {
+        EventId = Guid.NewGuid();
+        AffectedActorId = affectedActorId;
+    }
+
+    public ActorId AffectedActorId { get; }
+}
