c592c4bacbab7c6aae1102165b5ae07fc2fc006a
Jesper Justesen <1972142+FirestarJes@users.noreply.github.com>
2025-01-07 11:17:40 +0100

    Update 202501071058 insert new grid area for test.sql

--- source/marketparticipant/Energinet.DataHub.MarketParticipant.ApplyDBMigrationsApp/Scripts/TEST-001/Seed/202501071058 insert new grid area for test.sql
+++ source/marketparticipant/Energinet.DataHub.MarketParticipant.ApplyDBMigrationsApp/Scripts/TEST-001/Seed/202501071058 insert new grid area for test.sql
@@ -1,3 +1,3 @@
 INSERT INTO [dbo].[GridArea] ([Id], [Code], [Name], [Type], [PriceAreaCode], [ValidFrom], [FullFlexDate], [ChangedByIdentityId])
 VALUES
-    (NEWID(), '899', 'NET Visue Smile', 2, 1, CONVERT(datetime, '12-12-2023 00:00:00', 105), CONVERT(datetime, '12-12-2023 00:00:00', 105), '00000000-FFFF-FFFF-FFFF-000000000000')
+    (NEWID(), '899', 'Net VISUE SMILE', 2, 1, CONVERT(datetime, '12-12-2023 00:00:00', 105), CONVERT(datetime, '12-12-2023 00:00:00', 105), '00000000-FFFF-FFFF-FFFF-000000000000')
