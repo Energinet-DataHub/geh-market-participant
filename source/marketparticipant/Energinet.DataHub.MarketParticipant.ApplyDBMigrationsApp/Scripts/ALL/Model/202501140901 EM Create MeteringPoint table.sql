a231426e08e27c77b7ecc30857a2ddec29125f7c
Jesper Justesen <1972142+FirestarJes@users.noreply.github.com>
2025-01-15 11:12:17 +0100

    Removed uneeded scripts

--- source/marketparticipant/Energinet.DataHub.MarketParticipant.ApplyDBMigrationsApp/Scripts/ALL/Model/202501140901 EM Create MeteringPoint table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE [electricitymarket].[MeteringPoint]
-(
-    [Id]                 bigint IDENTITY(1,1) NOT NULL,
-    [Identification]     char(18) NOT NULL
-
-    CONSTRAINT PK_MeteringPoint PRIMARY KEY CLUSTERED (Id)
-)
--- source/marketparticipant/Energinet.DataHub.MarketParticipant.ApplyDBMigrationsApp/Scripts/ALL/Model/202501140907 EM Create MeteringPointEnergySuppliers view.sql
+++
@@ -1,9 +0,0 @@
-CREATE VIEW [electricitymarket].vw_MeteringPointEnergySuppliers AS
-SELECT
-    mp.Identification,
-    cr.EnergySupplier,
-    cr.StartDate,
-    cr.EndDate
-FROM [electricitymarket].[MeteringPoint] mp
-JOIN [electricitymarket].[CommercialRelation] cr ON mp.Id = cr.MeteringPointId
-WHERE cr.StartDate < cr.EndDate
