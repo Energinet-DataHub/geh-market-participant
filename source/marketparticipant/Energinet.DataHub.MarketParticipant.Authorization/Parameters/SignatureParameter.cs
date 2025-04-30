4e6a60720afba4f41c5151ded83022dd5092c07a
Jesper Justesen <1972142+FirestarJes@users.noreply.github.com>
2025-04-30 23:54:17 +0200

    Started working on restriction params

---
+++ source/marketparticipant/Energinet.DataHub.MarketParticipant.Authorization/Parameters/SignatureParameter.cs
@@ -0,0 +1,28 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace Energinet.DataHub.MarketParticipant.Authorization.Parameters;
+
+public abstract partial class SignatureParameter
+{
+    /// <summary>
+    /// Gets the data of the restriction value.
+    /// </summary>
+    public byte[] Data { get; }
+
+    /// <summary>
+    /// Key used to identify this signature parameter in the signature.
+    /// </summary>
+    public string Key { get; }
+}
