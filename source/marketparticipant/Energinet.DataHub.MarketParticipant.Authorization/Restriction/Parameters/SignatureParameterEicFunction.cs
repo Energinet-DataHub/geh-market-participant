1e3e10f11304b5c836289e8aed0ae8e0622c64c8
Jesper Justesen <1972142+FirestarJes@users.noreply.github.com>
2025-05-06 08:51:26 +0200

    Added EicFunction parameter

---
+++ source/marketparticipant/Energinet.DataHub.MarketParticipant.Authorization/Restriction/Parameters/SignatureParameterEicFunction.cs
@@ -0,0 +1,35 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Text;
+using Energinet.DataHub.MarketParticipant.Authorization.Model;
+
+namespace Energinet.DataHub.MarketParticipant.Authorization.Restriction.Parameters;
+
+public class SignatureParameterEicFunction : SignatureParameter
+{
+    // <summary>
+    // Initializes a new instance of the <see cref="SignatureParameterLong"/> for <see cref="long"/>class.
+    // </summary>
+    // <param name="value">The long value.</param>
+    internal SignatureParameterEicFunction(EicFunction value)
+    {
+        var nameBytes = Encoding.UTF8.GetBytes(value.GetType().Name);
+        var valueBytes = Encoding.UTF8.GetBytes(value.ToString());
+        ParameterData = nameBytes.Concat(valueBytes).ToArray();
+    }
+
+    // <inheritdoc />
+    internal override byte[] ParameterData { get; }
+}
