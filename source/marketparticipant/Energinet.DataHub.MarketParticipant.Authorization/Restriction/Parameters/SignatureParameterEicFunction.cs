1b317dc1a486761cbb0c676b29e70ca95ab65224
Jesper Justesen <jesper.justesen@gmail.com>
2025-05-08 15:02:07 +0200

    Update SignatureParameterEicFunction.cs

--- source/marketparticipant/Energinet.DataHub.MarketParticipant.Authorization/Restriction/Parameters/SignatureParameterEicFunction.cs
+++ source/marketparticipant/Energinet.DataHub.MarketParticipant.Authorization/Restriction/Parameters/SignatureParameterEicFunction.cs
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using System.Text;
 using Energinet.DataHub.MarketParticipant.Authorization.Model;
 
 namespace Energinet.DataHub.MarketParticipant.Authorization.Restriction.Parameters;
