16052e0fc24682f7c1fb8c3891001ee465e070f7
hjder-dev <henk.jan.de.ruiter@cgi.com>
2025-05-14 15:41:37 +0200

    Commit before merge changes of Main

--- source/marketparticipant/Energinet.DataHub.MarketParticipant.Authorization/Services/AccessValidators/MeteringPointMasterDataAccessValidation.cs
+++ source/marketparticipant/Energinet.DataHub.MarketParticipant.Authorization/Services/AccessValidators/MeteringPointMasterDataAccessValidation.cs
@@ -12,21 +12,46 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using System.Net;
+using System.Net.Http.Json;
+using System.Text.Json;
 using Energinet.DataHub.MarketParticipant.Authorization.AccessValidation;
 using Energinet.DataHub.MarketParticipant.Authorization.Model;
+using Energinet.DataHub.MarketParticipant.Authorization.Model.MasterData;
+using NodaTime;
+using NodaTime.Serialization.SystemTextJson;
 
 namespace Energinet.DataHub.MarketParticipant.Authorization.Services.AccessValidators;
 
-public sealed class MeteringPointMasterDataAccessValidation : IAccessValidator
+public sealed class MeteringPointMasterDataAccessValidation : IAccessValidator, IDisposable
 {
-    public MeteringPointMasterDataAccessValidation(MeteringPointMasterDataAccessValidationRequest validationRequest)
+    private readonly HttpClient _apiHttpClient;
+    private readonly JsonSerializerOptions _jsonSerializerOptions = new JsonSerializerOptions().ConfigureForNodaTime(DateTimeZoneProviders.Tzdb);
+    //internal MeteringPointMasterDataAccessValidation(HttpClient apiHttpClient)
+    //{
+    //    _apiHttpClient = apiHttpClient;
+    //}
+
+    //internal MeteringPointMasterDataAccessValidation()
+    //{
+    //    _apiHttpClient = new HttpClient();
+    //    _apiHttpClient.BaseAddress = new Uri("test.test");
+    //}
+    private MeteringPointMasterDataAccessValidation(MeteringPointMasterDataAccessValidationRequest validationRequest)
     {
         ArgumentNullException.ThrowIfNull(validationRequest);
 
         MarketRole = validationRequest.MarketRole;
+        _apiHttpClient = new HttpClient();
+        _apiHttpClient.BaseAddress = new Uri("test.test");
     }
 
     public EicFunction MarketRole { get; }
+
+    public void Dispose()
+    {
+        _apiHttpClient.Dispose();
+    }
 
     public bool Validate()
     {
@@ -42,8 +67,33 @@ public sealed class MeteringPointMasterDataAccessValidation : IAccessValidator
     {
         //TODO: Call elecitricity market
         var marketRole = MarketRole;
-        //USE GLN and market rol for look up Grid Area
+        //USE GLN and market role for look up Grid Area
         //lookup metering point to compare the registered grid
         return false;
     }
+
+    private async Task<IEnumerable<MeteringPointMasterData>> GetMeteringPointMasterDataChangesAsync(
+   MeteringPointIdentification meteringPointId,
+   Interval period)
+    {
+        ArgumentNullException.ThrowIfNull(meteringPointId);
+
+        var f = period.Start.ToDateTimeOffset();
+        var t = period.End.ToDateTimeOffset();
+
+        using var request = new HttpRequestMessage(HttpMethod.Post, "api/get-metering-point-master-data");
+        request.Content = JsonContent.Create(new MeteringPointMasterDataRequestDto(meteringPointId.Value, f, t));
+        using var response = await _apiHttpClient.SendAsync(request).ConfigureAwait(false);
+
+        if (response.StatusCode is HttpStatusCode.NotFound)
+            return [];
+
+        var result = await response.Content
+            .ReadFromJsonAsync<IEnumerable<MeteringPointMasterData>>(_jsonSerializerOptions)
+            .ConfigureAwait(false) ?? [];
+
+        return result;
+    }
+
+
 }
