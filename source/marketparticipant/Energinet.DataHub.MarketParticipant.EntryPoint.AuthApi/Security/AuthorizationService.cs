a02abea4b5b8ef4db7ffaf23dde6c2ab6939f4f7
Jesper Justesen <jesper.justesen@gmail.com>
2025-05-15 09:32:31 +0200

    Set expiration to 1 minute

--- source/marketparticipant/Energinet.DataHub.MarketParticipant.EntryPoint.AuthApi/Security/AuthorizationService.cs
+++ source/marketparticipant/Energinet.DataHub.MarketParticipant.EntryPoint.AuthApi/Security/AuthorizationService.cs
@@ -52,7 +52,7 @@ public class AuthorizationService
         if (!validator.Validate())
             throw new ArgumentException("CreateSignatureAsync: caller was not authorized to the requested resource");
 
-        var expires = DateTimeOffset.UtcNow.AddMinutes(15);
+        var expires = DateTimeOffset.UtcNow.AddMinutes(1);
         var signatureRequest = new SignatureRequest(expires);
         foreach (var signatureParam in accessValidationRequest.GetSignatureParams())
         {
