7ea056c4f0a9bb0eb10f764b415bb3ca2399256e
hjder-dev <henk.jan.de.ruiter@cgi.com>
2025-05-19 14:43:18 +0200

    Added new Energinet.DataHub.MarketParticipant.Authorization.Application

---
+++ source/marketparticipant/Energinet.DataHub.MarketParticipant.Authorization.Application/ElectricityMarket.cs
@@ -0,0 +1,56 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Net;
+using System.Net.Http.Json;
+using System.Text.Json;
+using Energinet.DataHub.MarketParticipant.Authorization.Application.Models.MasterData;
+using NodaTime;
+using NodaTime.Serialization.SystemTextJson;
+
+namespace Energinet.DataHub.MarketParticipant.Authorization.Application;
+
+public sealed class ElectricityMarket : IElectricityMarket
+{
+    private readonly HttpClient _apiHttpClient;
+    private readonly JsonSerializerOptions _jsonSerializerOptions = new JsonSerializerOptions().ConfigureForNodaTime(DateTimeZoneProviders.Tzdb);
+
+    internal ElectricityMarket(HttpClient apiHttpClient)
+    {
+        _apiHttpClient = apiHttpClient;
+    }
+
+    public async Task<IEnumerable<MeteringPointMasterData>> GetMeteringPointMasterDataAsync(
+        MeteringPointIdentification meteringPointId,
+        Interval period)
+    {
+        ArgumentNullException.ThrowIfNull(meteringPointId);
+
+        var f = period.Start.ToDateTimeOffset();
+        var t = period.End.ToDateTimeOffset();
+
+        using var request = new HttpRequestMessage(HttpMethod.Post, "api/get-metering-point-master-data");
+        request.Content = JsonContent.Create(new MeteringPointMasterDataRequestDto(meteringPointId.Value, f, t));
+        using var response = await _apiHttpClient.SendAsync(request).ConfigureAwait(false);
+
+        if (response.StatusCode is HttpStatusCode.NotFound)
+            return [];
+
+        var result = await response.Content
+            .ReadFromJsonAsync<IEnumerable<MeteringPointMasterData>>(_jsonSerializerOptions)
+            .ConfigureAwait(false) ?? [];
+
+        return result;
+    }
+}
---
+++ source/marketparticipant/Energinet.DataHub.MarketParticipant.Authorization.Application/Extensions/DependencyInjection/ElectricityMarketAuthApiModuleExtensions.cs
@@ -0,0 +1,71 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Identity;
+using Energinet.DataHub.MarketParticipant.Authorization.Application.Authorization;
+using Energinet.DataHub.MarketParticipant.Authorization.Application.Extensions.HealthChecks;
+using Energinet.DataHub.MarketParticipant.Authorization.Application.Options;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
+
+namespace Energinet.DataHub.MarketParticipant.Authorization.Application.Extensions.DependencyInjection;
+
+internal static class ElectricityMarketAuthApiModuleExtensions
+{
+    public static IServiceCollection AddElectricityMarketModule(this IServiceCollection services)
+    {
+        services
+            .AddOptions<ElectricityMarketClientOptions>()
+            .BindConfiguration(ElectricityMarketClientOptions.SectionName)
+            .ValidateDataAnnotations();
+
+        services.TryAddSingleton<IAuthorizationHeaderProvider>(sp =>
+        {
+            // We currently register AuthorizationHeaderProvider like this to be in control of the
+            // creation of DefaultAzureCredential.
+            // As we register IAuthorizationHeaderProvider as singleton, and it has the instance
+            // of DefaultAzureCredential, we expect it will use caching and handle token refresh.
+            // However, the documentation is a bit unclear: https://learn.microsoft.com/da-dk/dotnet/azure/sdk/authentication/best-practices?tabs=aspdotnet#understand-when-token-lifetime-and-caching-logic-is-needed
+            var credential = new DefaultAzureCredential();
+            var options = sp.GetRequiredService<IOptions<ElectricityMarketClientOptions>>().Value;
+            return new AuthorizationHeaderProvider(credential, options.ApplicationIdUri);
+        });
+
+        services.AddHttpClient("ElectricityMarketClient", (provider, client) =>
+        {
+            var options = provider.GetRequiredService<IOptions<ElectricityMarketClientOptions>>();
+            var headerProvider = provider.GetRequiredService<IAuthorizationHeaderProvider>();
+            client.DefaultRequestHeaders.Authorization = headerProvider.CreateAuthorizationHeader();
+            client.BaseAddress = options.Value.BaseUrl;
+        });
+
+        services.TryAddScoped<IElectricityMarket>(s =>
+        {
+            var client = s.GetRequiredService<IHttpClientFactory>().CreateClient("ElectricityMarketClient");
+            return new ElectricityMarket(client);
+        });
+
+        services.AddHealthChecks()
+          .AddElectricityMarketDataApiHealthCheck();
+
+        return services;
+    }
+}
